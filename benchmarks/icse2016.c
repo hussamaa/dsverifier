db893c7b96d21cddf94ad54ad0b6fa3c6177aa2a
Hussama Ismail <hussamaismail@gmail.com>
2015-08-05 21:28:57 -0400

    modifications in icse2016 benchmarks (fix)

--- benchmarks/icse2016.c
+++ benchmarks/icse2016.c
@@ -39,8 +39,8 @@ hardware hw = {
 	digital_system ds = { 
 		.b = { 1.5, -0.5 },
 		.b_size = 2,
-		.a = { 1.0 },
-		.a_size = 1,
+		.a = { 1.0, 0.0 },
+		.a_size = 2,
 		.sample_time = 0.02
 	};
 
@@ -99,8 +99,8 @@ hardware hw = {
 	digital_system ds = { 
 		.b = { 60.0, -50.0 },
 		.b_size = 2,
-		.a = { 1.0 },
-		.a_size = 1,
+		.a = { 1.0, 0.0 },
+		.a_size = 2,
 		.sample_time = 0.02
 	};
 
@@ -159,8 +159,8 @@ hardware hw = {
 	digital_system ds = { 
 		.b = { 110.0, -100.0 },
 		.b_size = 2,
-		.a = { 1.0 },
-		.a_size = 1,
+		.a = { 1.0, 0.0 },
+		.a_size = 2,
 		.sample_time = 0.02
 	};
 
@@ -220,8 +220,8 @@ hardware hw = {
 	digital_system ds = { 
 		.b = { 135.0, -260.0, 125.0 },
 		.b_size = 3,
-		.a = { 1.0, -1.0 },
-		.a_size = 2,
+		.a = { 1.0, -1.0, 0.0 },
+		.a_size = 3,
 		.sample_time = 0.02
 	};
 
@@ -280,8 +280,8 @@ hardware hw = {
 	digital_system ds = { 
 		.b = { 2002.0, -4000.0, 1998.0 },
 		.b_size = 3,
-		.a = { 1.0, -1.0 },
-		.a_size = 2,	
+		.a = { 1.0, 0.0, -1.0 },
+		.a_size = 3,	
 		.sample_time = 0.001
 	};
 
