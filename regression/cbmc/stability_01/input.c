30bd94c6d416a26155e2b70888421457969aa9b2
Hussama Ismail <hussamaismail@gmail.com>
2015-08-11 22:23:26 -0400

    fixing test cases for cbmc

--- regression/cbmc/stability_01/input.c
+++ regression/cbmc/stability_01/input.c
@@ -4,7 +4,7 @@ digital_system ds = {
         .a = { 1.0, 1.0, 0.31, 0.03 },       
         .a_size = 4,
         .b = { 1.0, 6.0, 1.0, 6.0 },
-        .b_size = 4,
+        .b_size = 4
 };
 
 implementation impl = {
@@ -13,5 +13,5 @@ implementation impl = {
         .delta = 0.25,
         .max = 1.0,
         .min = -1.0,
-        .scale = 1e6
+        .scale = 1000000
 };
--- regression/cbmc/stability_02/input.c
+++ regression/cbmc/stability_02/input.c
@@ -1,10 +1,10 @@
 #include "../../../bmc/dsverifier.h"
 
 digital_system ds = {
-        .a = { 1.0, 1.0, -17.0, 15 },       
+        .a = { 1.0, 1.0, -17.0, 15.0 },       
         .a_size = 4,
         .b = { 1.0, 6.0, 1.0, 6.0 },
-        .b_size = 4,
+        .b_size = 4
 };
 
 implementation impl = {
@@ -13,5 +13,5 @@ implementation impl = {
         .delta = 0.25,
         .max = 1.0,
         .min = -1.0,
-        .scale = 1e6
+        .scale = 1000000
 };
