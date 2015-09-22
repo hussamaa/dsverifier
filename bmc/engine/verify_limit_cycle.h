cb262dd9e77679e2e144f417c936e50f802ff16f
Hussama Ismail <hussamaismail@gmail.com>
2015-09-21 22:08:43 -0400

    adjusting lc to use delta with base

--- bmc/engine/verify_limit_cycle.h
+++ bmc/engine/verify_limit_cycle.h
@@ -33,7 +33,7 @@ int verify_limit_cycle(void){
 	#elif ((REALIZATION == DDFI)||(REALIZATION == DDFII)||(REALIZATION == TDDFII))
 		double da[ds.a_size];
 		double db[ds.b_size];
-		get_delta_transfer_function(ds.b, db, ds.b_size,ds.a, da, ds.a_size, impl.delta);
+		get_delta_transfer_function_with_base(ds.b, db, ds.b_size,ds.a, da, ds.a_size, impl.delta);
 		fxp32_t a_fxp[ds.a_size];
 		fxp32_t b_fxp[ds.b_size];
 		/* quantize delta denominators using fxp */
