b8b7710d62eea147ac670e51ed8a797edf687b43
root <hussamaismail@gmail.com>
2015-10-30 23:57:49 -0400

    including test cases for lco in fxp

---
+++ regression/limit-cycle/check_fxp_limit_cycle_01/input.c
@@ -0,0 +1,11 @@
+#include "../../../bmc/core/definitions.h"
+#include "../../../bmc/core/compatibility.h"
+#include "../../../bmc/core/fixed-point.h"
+#include "../../../bmc/core/functions.h"
+#include "../../../bmc/core/util.h"
+
+int main(){
+	fxp32_t y[17] = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+	int y_size = 17;
+	fxp_check_persistent_limit_cycle(y, y_size);
+}
