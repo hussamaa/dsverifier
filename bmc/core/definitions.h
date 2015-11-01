00eab64b72627928d8d312b3604f43a59e98081b
Hussama Ismail <hussamaismail@gmail.com>
2015-10-31 21:30:25 -0400

    refactoring definitions.h

--- bmc/core/definitions.h
+++ bmc/core/definitions.h
@@ -10,115 +10,116 @@
  *
  * ------------------------------------------------------
  *
- * definitions file
+ * This file contains the definitions for DSVerifier and
+ * its properties verification. Here, we have the available
+ * realizations, properties constants, overflow handling,
+ * rounding mode, input and output formats, and etc.
  *
  * ------------------------------------------------------
 */
 
-#define LIMIT 3
-
-/* model checkers */
-#define NONE  0
-#define ESBMC 1
-#define CBMC  2
+/** model checkers */
+#define NONE                                0
+#define ESBMC                               1
+#define CBMC                                2
 #ifndef BMC
-	#define BMC	1
+	#define BMC	                            1
 #endif
 
-/* realizations (use: -DREALIZATION=DIRECTFORMI) */
-#define DIRECTFORMI							1
-#define DIRECTFORMII						2
-#define TRANSPOSEDDIRECTFORMII				3
-#define DELTADIRECTFORMI					4
-#define DELTADIRECTFORMII					5
-#define DELTATRANSPOSEDDIRECTFORMII			6
-#define DIRECTFORMICASCADE   				7
-#define DIRECTFORMIICASCADE   				8
-#define TRANSPOSEDDIRECTFORMIICASCADE   	9
-#define DELTADIRECTFORMICASCADE   			10
-#define DELTADIRECTFORMIICASCADE   			11
-#define DELTATRANSPOSEDDIRECTFORMIICASCADE 	12
+/** available realizations */
+#define DIRECTFORMI                         1
+#define DIRECTFORMII                        2
+#define TRANSPOSEDDIRECTFORMII              3
+#define DELTADIRECTFORMI                    4
+#define DELTADIRECTFORMII                   5
+#define DELTATRANSPOSEDDIRECTFORMII         6
+#define DIRECTFORMICASCADE                  7
+#define DIRECTFORMIICASCADE                 8
+#define TRANSPOSEDDIRECTFORMIICASCADE       9
+#define DELTADIRECTFORMICASCADE             10
+#define DELTADIRECTFORMIICASCADE            11
+#define DELTATRANSPOSEDDIRECTFORMIICASCADE  12
 
-/* nickname for realizations (use: -DREALIZATION=DFI) */
-#define DFI									1
-#define DFII								2
-#define TDFII								3
-#define DDFI            					4
-#define DDFII								5
-#define TDDFII								6
-#define CDFI            					7
-#define CDFII								8
-#define CTDFII								9
-#define CDDFI            					10
-#define CDDFII								11
-#define CTDDFII								12
+/** nickname for realizations */
+#define DFI                                 1
+#define DFII                                2
+#define TDFII                               3
+#define DDFI                                4
+#define DDFII                               5
+#define TDDFII                              6
+#define CDFI                                7
+#define CDFII                               8
+#define CTDFII                              9
+#define CDDFI                               10
+#define CDDFII                              11
+#define CTDDFII                             12
 
-/* property verification (use: -DPROPERTY=OVERFLOW) */
-#define NOTHING 							0
-#define OVERFLOW 							1
-#define ZERO_INPUT_LIMIT_CYCLE				2
-#define LIMIT_CYCLE 						3
-#define TIMING_MSP430						4
-#define TIMING      						5
-#define ERROR								6
-#define STABILITY 							7
-#define MINIMUM_PHASE 						8
-#define STABILITY_CLOSED_LOOP				9
-#define LIMIT_CYCLE_CLOSED_LOOP				10
-#define ERROR_STATE_SPACE					11
+/** property verification */
+#define NOTHING                             0
+#define OVERFLOW                            1
+#define ZERO_INPUT_LIMIT_CYCLE              2
+#define LIMIT_CYCLE                         3
+#define TIMING_MSP430                       4
+#define TIMING                              5
+#define ERROR                               6
+#define STABILITY                           7
+#define MINIMUM_PHASE                       8
+#define STABILITY_CLOSED_LOOP               9
+#define LIMIT_CYCLE_CLOSED_LOOP             10
+#define ERROR_STATE_SPACE                   11
 
-/** Check Required Parameters */
+/** required parameters */
 #ifndef PROPERTY
-	#define PROPERTY 						0
+	#define PROPERTY                        0
 #endif
 #ifndef REALIZATION
-	#define REALIZATION 					0
+	#define REALIZATION                     0
 #endif
 #ifndef X_SIZE
-	#define X_SIZE 							0
+	#define X_SIZE                          0
 #endif
 #ifndef K_SIZE
-	#define K_SIZE 							0
+	#define K_SIZE                          0
 #endif
 #ifndef EXPECTED_ERROR
-	#define EXPECTED_ERROR 					-1
+	#define EXPECTED_ERROR                  -1
 #endif
 
-/* processor parameters (OBSOLETE) */
-#define CLOCK								16000000
-#define CYCLE								1 / CLOCK
+/** processor parameters (DEPRECATED) */
+#define CLOCK                               16000000
+#define CYCLE                               1 / CLOCK
 #ifndef SAMPLERATE
-	#define SAMPLERATE 						100
+	#define SAMPLERATE                      100
 #endif
-#define DEADLINE 							1 / SAMPLERATE
-#define OVERHEAD							0
+#define DEADLINE                            1 / SAMPLERATE
+#define OVERHEAD                            0
 
-/* overflow mode */
-#define DETECT_OVERFLOW						1
-#define SATURATE							2
-#define WRAPAROUND							3
+/** overflow mode */
+#define DETECT_OVERFLOW                     1
+#define SATURATE                            2
+#define WRAPAROUND                          3
 
 /** rounding mode */
-#define ROUNDING							0
-#define FLOOR								1
-#define CEIL								2
+#define ROUNDING                            0
+#define FLOOR                               1
+#define CEIL                                2
 
-/* overflow, x_size, and round parameters */
+/** overflow, x_size, and round parameters */
 int X_SIZE_VALUE = 0;
-int OVERFLOW_MODE = 1; 						/* DETECT_OVERFLOW */
-int ROUNDING_MODE = 0;
+int OVERFLOW_MODE = DETECT_OVERFLOW;
+int ROUNDING_MODE = ROUNDING;
 
-/* connection mode for control + model (use: -DCONNECTION_MODE=SERIES) */
-#define SERIES 								1
-#define FEEDBACK 							2
+/** connection mode for closed-loop verification */
+#define SERIES                              1
+#define FEEDBACK                            2
 #ifndef CONNECTION_MODE
-	#define CONNECTION_MODE					0
+	#define CONNECTION_MODE                 0
 #endif
 
-/* device model */
-#define MSP430								1
+/** device model */
+#define MSP430                              1
 
-/* digital system structure */
+/* digital system in transfer function structure */
 typedef struct {
   double a[100];
   int a_size;
@@ -129,6 +130,8 @@ typedef struct {
   double b_uncertainty[100];
 } digital_system;
 
+/** digital system in space state structure */
+#define LIMIT                               3
 typedef struct {
     double A[LIMIT][LIMIT];
     unsigned int rowA;
@@ -153,7 +156,7 @@ typedef struct {
     unsigned int columnInputs;
 } digital_system_state_space;
 
-/* implementation structure */
+/** implementation structure */
 typedef struct {
    int int_bits;
    int frac_bits;
@@ -165,7 +168,7 @@ typedef struct {
    double max_error;
 } implementation;
 
-/* assembly structure */
+/** assembly structure */
 typedef struct {
 	int push;
 	int in;
@@ -197,7 +200,7 @@ typedef struct {
 	int cpi;
 } instructions;
 
-/* hardware structure */
+/** hardware structure */
 typedef struct {
    long clock;
    int device;
