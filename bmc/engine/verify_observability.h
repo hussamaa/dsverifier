7313da2d0014549177855106c4a641f115c055cd
Felipe Monteiro <rms.felipe@gmail.com>
2016-03-16 18:00:31 -0300

    adding support to verify observability of MIMO systems
    Signed-off-by: Felipe Monteiro <rms.felipe@gmail.com>

--- bmc/engine/verify_observability.h
+++ bmc/engine/verify_observability.h
@@ -11,10 +11,14 @@
 # ------------------------------------------------------
 */
 
+//#include <stdio.h>
+//#include <stdlib.h>
+
 extern digital_system_state_space _controller;
 
 int verify_observability(void){
 
+	//setting up variables
 	int i;
 	int j;
 
@@ -25,6 +29,7 @@ int verify_observability(void){
     fxp_t backupSecond[LIMIT][LIMIT];
 	double observabilityMatrix_double[LIMIT][LIMIT];
 
+	//initializing variables
 	for(i=0; i<nStates;i++){
 		for(j=0; j<nStates;j++){
 			observabilityMatrix[i][j]= 0;
@@ -35,6 +40,7 @@ int verify_observability(void){
 		}
 	}
 
+	//converting A and C matrix to fixed point
 	for(i=0; i<nStates;i++){
 		for(j=0; j<nStates;j++){
 			A_fpx[i][j]= fxp_double_to_fxp(_controller.A[i][j]);
@@ -47,21 +53,86 @@ int verify_observability(void){
 		}
 	}
 
-	for(i=0; i<nStates;i++){
-		fxp_exp_matrix(nStates,nStates,A_fpx,i,backup);
-		fxp_matrix_multiplication(nOutputs,nStates,nStates,nStates,C_fpx,backup,backupSecond);
-		for(j = 0; j<nStates;j++){
-				observabilityMatrix[i][j]= backupSecond[0][j];
+	if(nOutputs > 1){ // checking if it is a MIMO system
+		int l;
+		j = 0;
+		for(l=0; l<nStates;){//calculating observability matrix from the MIMO system
+			fxp_exp_matrix(nStates,nStates,A_fpx,l,backup);
+			l++;
+			fxp_matrix_multiplication(nOutputs,nStates,nStates,nStates,C_fpx,backup,backupSecond);
+			for(int k = 0; k < nOutputs; k++){
+				for(i = 0; i<nStates;i++){
+						observabilityMatrix[j][i]= backupSecond[k][i];
+				}
+				j++;
+			}
 		}
-	}
-
-	for(i=0; i<nStates;i++){
-		for(j=0; j<nStates;j++){
-			observabilityMatrix_double[i][j]= fxp_to_double(observabilityMatrix[i][j]);
+/*
+		for(i=0; i<(nStates*nOutputs);i++){
+			for(j=0; j<nStates;j++){
+				observabilityMatrix_double[i][j]= fxp_to_double(observabilityMatrix[i][j]);
+			}
 		}
-	}
 
-	assert(determinant(observabilityMatrix_double,nStates) != 0);
+		printf("#matrix: OBSERVABILITY MATRIX:\n\n");
+		print_matrix(observabilityMatrix_double,(nStates*nOutputs),nStates);
+*/
+		for(i=0; i<nStates;i++){
+			for(j=0; j<(nStates*nOutputs);j++){
+				backup[i][j]= 0.0;
+			}
+		}
+
+		//Calculating transpose matrix
+		fxp_transpose(observabilityMatrix,backup,(nStates*nOutputs),nStates);
+/*
+		for(i=0; i<nStates;i++){
+			for(j=0; j<(nStates*nOutputs);j++){
+				observabilityMatrix_double[i][j]= fxp_to_double(backup[i][j]);
+			}
+		}
+
+		printf("#matrix: TRANSPOSE OBSERVABILITY MATRIX:\n\n");
+		print_matrix(observabilityMatrix_double,nStates,(nStates*nOutputs));
+*/
+		//Calculating O'*O
+		fxp_t mimo_observabilityMatrix_fxp[LIMIT][LIMIT];
+		fxp_matrix_multiplication(nStates,(nStates*nOutputs),(nStates*nOutputs),nStates,backup,observabilityMatrix,mimo_observabilityMatrix_fxp);
+/*
+		for(i=0; i<nStates;i++){
+			for(j=0; j<nStates;j++){
+				observabilityMatrix_double[i][j]= fxp_to_double(mimo_observabilityMatrix_fxp[i][j]);
+			}
+		}
+
+		printf("#matrix: FINAL MATRIX:\n\n");
+		print_matrix(observabilityMatrix_double,nStates,nStates);
+*/
+		//Converting controllability matrix from fixed point to double
+		for(i=0; i<nStates;i++){
+			for(j=0; j<nStates;j++){
+				observabilityMatrix_double[i][j]= fxp_to_double(mimo_observabilityMatrix_fxp[i][j]);
+			}
+		}
+
+		//Calculating determinant
+		assert(determinant(observabilityMatrix_double,nStates) != 0);
+	}else{
+		for(i=0; i<nStates;i++){
+			fxp_exp_matrix(nStates,nStates,A_fpx,i,backup);
+			fxp_matrix_multiplication(nOutputs,nStates,nStates,nStates,C_fpx,backup,backupSecond);
+			for(j = 0; j<nStates;j++){
+					observabilityMatrix[i][j]= backupSecond[0][j];
+			}
+		}
+
+		for(i=0; i<nStates;i++){
+			for(j=0; j<nStates;j++){
+				observabilityMatrix_double[i][j]= fxp_to_double(observabilityMatrix[i][j]);
+			}
+		}
+		assert(determinant(observabilityMatrix_double,nStates) != 0);
+	}
 
 	return 0;
 }
