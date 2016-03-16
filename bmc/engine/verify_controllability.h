f6a0706fe10eb82e7b990a9a6a0bb2e6592147e5
Felipe Monteiro <rms.felipe@gmail.com>
2016-03-16 15:33:38 -0300

    updating controllability verification
    Signed-off-by: Felipe Monteiro <rms.felipe@gmail.com>

--- bmc/engine/verify_controllability.h
+++ bmc/engine/verify_controllability.h
@@ -15,6 +15,7 @@ extern digital_system_state_space _controller;
 
 int verify_controllability(void){
 
+	// setting up variables
 	int i;
 	int j;
 
@@ -23,7 +24,9 @@ int verify_controllability(void){
 	fxp_t controllabilityMatrix[LIMIT][LIMIT];
 	fxp_t backup[LIMIT][LIMIT];
 	fxp_t backupSecond[LIMIT][LIMIT];
+	double controllabilityMatrix_double[LIMIT][LIMIT];
 
+	//initializing variables
 	for(i=0; i<nStates;i++){
 		for(j=0; j<(nStates*nInputs);j++){
 			A_fpx[i][j] = 0.0;
@@ -31,23 +34,26 @@ int verify_controllability(void){
 			controllabilityMatrix[i][j] = 0.0;
 			backup[i][j] = 0.0;
 			backupSecond[i][j] = 0.0;
+			controllabilityMatrix_double[i][j] = 0.0;
 		}
 	}
 
+	//converting A and B matrix to fixed point
 	for(i=0; i<nStates;i++){
 		for(j=0; j<nStates;j++){
-			A_fpx[i][j]= (_controller.A[i][j]);
+			A_fpx[i][j]= fxp_double_to_fxp(_controller.A[i][j]);
 		}
 	}
 
 	for(i=0; i<nStates;i++){
 		for(j=0; j<nInputs;j++){
-			B_fpx[i][j]= (_controller.B[i][j]);
+			B_fpx[i][j]= fxp_double_to_fxp(_controller.B[i][j]);
 		}
 	}
 
-	if(nInputs > 1){
+	if(nInputs > 1){ // checking if it is a MIMO system
 		int l = 0;
+		//calculating controllability matrix from the MIMO system
 		for(j=0; j<(nStates*nInputs);){
 			fxp_exp_matrix(nStates,nStates,A_fpx,l,backup);
 			l++;
@@ -66,12 +72,28 @@ int verify_controllability(void){
 			}
 		}
 
+		//Calculating transpose matrix
 		fxp_transpose(controllabilityMatrix,backup,nStates,(nStates*nInputs));
 
+		//Calculating C*C'
 		fxp_t mimo_controllabilityMatrix_fxp[LIMIT][LIMIT];
 		fxp_matrix_multiplication(nStates,(nStates*nInputs),(nStates*nInputs),nStates,controllabilityMatrix,backup,mimo_controllabilityMatrix_fxp);
-		assert(fxp_determinant(mimo_controllabilityMatrix_fxp,nStates) != 0);
+
+		//Converting controllability matrix from fixed point to double
+		for(i=0; i<nStates;i++){
+			for(j=0; j<nStates;j++){
+				controllabilityMatrix_double[i][j]= fxp_to_double(mimo_controllabilityMatrix_fxp[i][j]);
+			}
+		}
+
+		//Calculating determinant
+		//assert(fxp_determinant(mimo_controllabilityMatrix_fxp,nStates) != 0);
+		assert(fxp_determinant(controllabilityMatrix_double,nStates) != 0);
 	} else {
+
+		// Checking a SISO system
+
+		//Calculating controllability matrix
 		for(j=0; j<nStates;j++){
 			fxp_exp_matrix(nStates,nStates,A_fpx,j,backup);
 			fxp_matrix_multiplication(nStates,nStates,nStates,nInputs,backup,B_fpx,backupSecond);
@@ -79,12 +101,22 @@ int verify_controllability(void){
 					controllabilityMatrix[i][j]= backupSecond[i][0];
 			}
 		}
-		assert(fxp_determinant(controllabilityMatrix,nStates) != 0);
+
+		//Converting controllability matrix from fixed point to double
+		for(i=0; i<nStates;i++){
+			for(j=0; j<nStates;j++){
+				controllabilityMatrix_double[i][j]= fxp_to_double(controllabilityMatrix[i][j]);
+			}
+		}
+
+		//Calculating determinant
+		assert(determinant(controllabilityMatrix_double,nStates) != 0);
 	}
 
 	return 0;
 }
 
+//This version performs all operations using only double values
 int verify_controllability_double(void){
 
 	int i;
