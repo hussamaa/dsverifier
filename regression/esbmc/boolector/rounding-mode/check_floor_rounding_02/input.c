f3e423ca8d61bdc338fa44d692468672a6d848a5
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-08-25 16:17:46 +0100

    restructure rounding-mode test cases
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/esbmc/boolector/rounding-mode/check_floor_rounding_02/input.c
@@ -0,0 +1,48 @@
+#include <dsverifier.h>
+
+digital_system ds = {
+	.b = { -1.553, 3.119, -1.566 },
+	.b_size = 3,
+	.a = { 1.00000000, 0.0387300, -0.96 },
+	.a_size = 3,
+	.sample_time = 0.005
+};
+
+implementation impl = {
+	.int_bits = 15,
+	.frac_bits = 8,
+	.min = -6.0,
+	.max = 6.0
+};
+
+int unit_test(){
+
+	initialization();
+
+	OVERFLOW_MODE = WRAPAROUND;
+	ROUNDING_MODE = FLOOR;
+
+	fxp_t a_fxp[ds.a_size];
+	fxp_t b_fxp[ds.b_size];
+
+	/* quantize the numerator using fxp */
+	fxp_double_to_fxp_array(ds.b, b_fxp, ds.b_size);
+
+	/* quantize the denominator using fxp */
+	fxp_double_to_fxp_array(ds.a, a_fxp, ds.a_size);
+
+	double b_qtz[ds.b_size];
+	double a_qtz[ds.a_size];
+
+	fxp_to_double_array(b_qtz, b_fxp, ds.b_size);
+	fxp_to_double_array(a_qtz, a_fxp, ds.a_size);
+
+	__DSVERIFIER_assert(b_qtz[0] == -1.5546875);
+	__DSVERIFIER_assert(b_qtz[1] == 3.1171875);
+	__DSVERIFIER_assert(b_qtz[2] == -1.56640625);
+
+	__DSVERIFIER_assert(a_qtz[0] == 1.0);
+	__DSVERIFIER_assert(a_qtz[1] == 0.03515625);
+	__DSVERIFIER_assert(a_qtz[2] == -0.9609375);
+
+}
