f7dde1dad4bf94f57e1828c42a8a24d47afa0444
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-08-25 12:18:47 +0100

    restructure test cases
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc/boolector/check_limit_cycle_03/input.c
+++
@@ -1,8 +0,0 @@
-#include "../../../../bmc/core/compatibility.h"
-#include "../../../../bmc/core/util.h"
-
-int main(){
-	double y[6] = { -0.5, 0.5, -0.5, 0.5, -0.5, 0.5 };
-	int y_size = 6;
-	double_check_limit_cycle(y, y_size);
-}
