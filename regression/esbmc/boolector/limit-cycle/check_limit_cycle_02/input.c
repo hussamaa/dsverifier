03a7ce3c9e91b592245e06c50ce30f36cf0c60f5
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-08-29 12:16:57 +0100

    remove test cases
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc/boolector/limit-cycle/check_limit_cycle_02/input.c
+++
@@ -1,7 +0,0 @@
-#include <dsverifier.h>
-
-int unit_test(){
-	double y[17] = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 150, 2, 150 };
-	int y_size = 17;
-	double_check_persistent_limit_cycle(y, y_size);
-}
--- regression/esbmc/boolector/limit-cycle/check_limit_cycle_06/input.c
+++
@@ -1,7 +0,0 @@
-#include <dsverifier.h>
-
-int unit_test(){
-	double y[10] = { 2, 2, 2, 4, 4, 4, 2, 2, 2, 4};
-	int y_size = 10;
-	double_check_persistent_limit_cycle(y, y_size);
-}
