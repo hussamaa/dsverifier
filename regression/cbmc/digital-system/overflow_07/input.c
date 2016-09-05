39b0a3b761f47deb7f04e022870648df0f5e6771
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-09-05 15:47:18 +0100

    add new test case related to overflow
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/cbmc/digital-system/overflow_07/input.c
@@ -0,0 +1,18 @@
+#include <dsverifier.h>
+
+digital_system ds = { 
+	.b = {0.15f, 0.05f, 0.4f}, /* digital system numerator coefficients */
+	.b_size = 3,
+	.a = {1.0f, 0.0f, 0.3f}, /* digital system denominator coefficients */
+	.a_size = 3,
+	.sample_time = 0.02
+};
+
+implementation impl = { 
+	.int_bits = 3,
+	.frac_bits = 4,
+	.delta = 0.125,
+	.max = 1.0,
+	.min = -1.0,
+};
+
