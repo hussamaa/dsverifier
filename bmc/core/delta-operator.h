aaf52415aaa5161139e3fb41631e4fdb8ae09d45
lennonchaves <lennon.correach@gmail.com>
2016-09-13 10:28:22 -0400

    Implementation of nchoosek function in C

--- bmc/core/delta-operator.h
+++ bmc/core/delta-operator.h
@@ -69,16 +69,42 @@ void get_delta_transfer_function(double b[], double b_out[], int b_size, double 
 	generate_delta_coefficients(a, a_out, a_size, delta);
 }
 
-/* get a transfer function in delta domain using as base the first denominator element */
+/*returns the binomial coefficient, defined as n!/((n–k)! k!). This is the number of combinations of n items taken k at a time.*/
+int nchoosek(int n, int k){
+
+if (k == 0) 
+  return 1;
+
+return (n * nchoosek(n - 1, k - 1)) / k;
+
+}
+
+/* get a digital system represented in transfer function and transform in delta form */
 void get_delta_transfer_function_with_base(double b[], double b_out[], int b_size, double a[], double a_out[], int a_size, double delta){
-	get_delta_transfer_function(b, b_out, b_size, a, a_out, a_size, delta);
-	int i = 0;
-	double base = a_out[0];
-	/** applying base in numerator coefficients */
-	for (i=0; i < b_size; i++){
-		b_out[i] = b_out[i] / base;
+	int i,j;
+	int N = a_size - 1;
+	int M = b_size - 1;
+	double sum_delta_operator;
+
+	/*delta form for denominator*/
+	for(i=0; i<=N; i++)
+	{
+	sum_delta_operator = 0;
+	  for(j=0; j<=i; j++)
+ 	  {
+		sum_delta_operator = sum_delta_operator + a[j]*nchoosek(N-j,i-j);
+	  }
+	a_out[i] = pow(delta,N-i)*sum_delta_operator;
 	}
-	for (i=0; i < a_size; i++){
-		a_out[i] = a_out[i] / base;
+
+	/*delta form for numerator*/
+	for(i=0; i<=M; i++)
+	{
+	sum_delta_operator = 0;
+	  for(j=0; j<=i; j++)
+ 	  {
+		sum_delta_operator = sum_delta_operator + b[j]*nchoosek(M-j,i-j);
+	  }
+	b_out[i] = pow(delta,M-i)*sum_delta_operator;
 	}
 }
