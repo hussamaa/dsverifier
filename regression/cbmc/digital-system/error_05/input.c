178272838a9e300a4671e4580c20b4c1c13e7011
Lucas Cordeiro <lucasccordeiro@gmail.com>
2016-09-24 22:43:03 +0100

    add test cases related to verify_error
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/cbmc/digital-system/error_05/input.c
@@ -0,0 +1,18 @@
+#include <dsverifier.h>
+
+digital_system ds = {
+	.b = { 0.2, -0.4, 0.2 },
+	.b_size = 3,
+	.a = { 1.0, 0.0, -0.25 },
+	.a_size = 3,
+	.sample_time = 0.01
+};
+
+implementation impl = {
+	.int_bits = 3,
+	.frac_bits = 4,
+	.max_error = 10,
+	.min = -1.0,
+	.max = 1.0
+};
+
