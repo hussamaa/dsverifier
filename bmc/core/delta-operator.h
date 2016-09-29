da30d6dc5a7b55b0b82b47b143bb08534e7e3e48
lennonchaves <lennon.correach@gmail.com>
2016-09-29 00:28:33 -0400

    Updating generate_delta_coefficients function to be implemented according to the new implementation for delta transformation.

--- bmc/core/delta-operator.h
+++ bmc/core/delta-operator.h
@@ -19,56 +19,6 @@
 #include <stdlib.h>
 #include <assert.h>
 
-int binomial_coefficient(int n, int p){
-   return fatorial(n) / (fatorial(p) * fatorial(n-p));
-}
-
-void binomial(int degree, double delta, double out[]){
-   initialize_array(out, 3);
-   int i;
-   for(i=0; i<=degree; i++){
-	  out[degree-i] = binomial_coefficient(degree, i) * internal_pow(delta, degree-i);
-   }
-}
-
-/** calculate the delta coefficients for a polynomial */
-void generate_delta_coefficients(double vetor[], double out[], int n, double delta){
-   initialize_array(out,n);
-   double a_invertido[n];
-   initialize_array(a_invertido,n);
-   revert_array(vetor, a_invertido, n);
-   double _a[n];
-   initialize_array(_a,n);
-   int i,j;
-   for(i=0; i < n; i++){
-	  double b[n+1];
-	  initialize_array(b,n+1);
-	  binomial(i, delta, b);
-	  for(j=0; j<i+1; j++){
-		 b[j] = b[j] * a_invertido[i];
-		 _a[j] = _a[j] + b[j];
-	  }
-   }
-   revert_array(_a, out, n);
-}
-
-/** calculate the delta coefficients for a polynomial */
-void generate_delta_coefficients_with_base(double vetor[], double out[], int n, double delta){
-	generate_delta_coefficients(vetor, out, n, delta);
-	int i;
-	double base = out[0];
-	for(i=0; i<n; i++){
-		out[i] = out[i] / base;
-	}
-}
-
-/* get a transfer function in delta domain */
-void get_delta_transfer_function(double b[], double b_out[], int b_size, double a[], double a_out[], int a_size, double delta){
-	/* generate delta coefficients */
-	generate_delta_coefficients(b, b_out, b_size, delta);
-	generate_delta_coefficients(a, a_out, a_size, delta);
-}
-
 /*returns the binomial coefficient, defined as n!/((n–k)! k!). This is the number of combinations of n items taken k at a time.*/
 int nchoosek(int n, int k){
 
@@ -77,6 +27,32 @@ if (k == 0)
 
 return (n * nchoosek(n - 1, k - 1)) / k;
 
+}
+
+/** calculate the delta coefficients for a polynomial */
+void generate_delta_coefficients(double vetor[], double out[], int n, double delta){
+	int i,j;
+	int N = n - 1;
+	double sum_delta_operator;
+
+	/*delta form for polynomial*/
+	for(i=0; i<=N; i++)
+	{
+	sum_delta_operator = 0;
+	  for(j=0; j<=i; j++)
+ 	  {
+		sum_delta_operator = sum_delta_operator + vetor[j]*nchoosek(N-j,i-j);
+	  }
+	out[i] = internal_pow(delta,N-i)*sum_delta_operator;
+	}
+}
+
+
+/* get a transfer function in delta domain */
+void get_delta_transfer_function(double b[], double b_out[], int b_size, double a[], double a_out[], int a_size, double delta){
+	/* generate delta coefficients */
+	generate_delta_coefficients(b, b_out, b_size, delta);
+	generate_delta_coefficients(a, a_out, a_size, delta);
 }
 
 /* get a digital system represented in transfer function and transform in delta form */
