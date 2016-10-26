271767158de326fdbd4152696799219f42fd376b
lennonchaves <lennon.correach@gmail.com>
2016-10-26 12:51:53 -0400

    adding limit cycle and quantization error for state space

--- matlab-toolbox/toolbox/verify_ss_limit_cycle.m
+++ matlab-toolbox/toolbox/verify_ss_limit_cycle.m
@@ -1,12 +1,12 @@
-function verify_ss_quantization_error(system, bmc, realization, solver, xsize, error)
+function verify_ss_limit_cycle(system, bmc, realization, solver, xsize)
 
 global property;
 %setting the DSVERIFIER_HOME
 dsv_setup();
 %translate to ANSI-C file
-dsv_parser(system,'ss',error);
+dsv_parser(system,'ss',0);
 %verifying using DSVerifier command-line
-property = 'QUANTISATION_ERROR';
+property = 'LIMIT_CYCLE_STATE_SPACE';
 command_line = [' --property ' property ' --realization ' realization ' --x-size ' num2str(xsize) ' --bmc ' bmc ' --solver ' solver];
 dsv_verification(command_line,'ss');
 %report the verification
