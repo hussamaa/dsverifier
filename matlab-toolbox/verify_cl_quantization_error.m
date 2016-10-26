d6634100b2d0bef90123ec8af40500d255027ac9
lennonchaves <lennon.correach@gmail.com>
2016-10-26 01:27:23 -0400

    Closed-loop verification in matlab

--- matlab-toolbox/verify_cl_quantization_error.m
+++ matlab-toolbox/verify_cl_quantization_error.m
@@ -1,4 +1,21 @@
-function output = verify_cl_quantization_error(system, bmc, realization, solver, xsize, c_mode, error)
+function verify_cl_quantization_error(system, bmc, realization, solver, xsize, c_mode, error)
+
+global property;
+
+%setting the DSVERIFIER_HOME
+dsv_setup();
+
+%translate to ANSI-C file
+dsv_parser(system,'cl', error);
+
+%verifying using DSVerifier command-line
+property = 'QUANTIZATION_ERROR_CLOSED_LOOP';
+command_line = [' --property ' property ' --realization ' realization ' --x-size ' num2str(xsize) ' --bmc ' bmc ' --solver ' solver ' --connection-mode ' c_mode];
+dsv_verification(command_line);
+
+%report the verification
+output = dsv_report('output.out');
+disp(output);
 
 end
 
--- matlab-toolbox/verify_cl_stability.m
+++ matlab-toolbox/verify_cl_stability.m
@@ -1,4 +1,21 @@
-function output = verify_cl_stability(system, bmc, realization, solver, xsize, c_mode)
+function verify_cl_stability(system, bmc, realization, solver, xsize, c_mode)
+
+global property;
+
+%setting the DSVERIFIER_HOME
+dsv_setup();
+
+%translate to ANSI-C file
+dsv_parser(system,'cl',0);
+
+%verifying using DSVerifier command-line
+property = 'STABILITY_CLOSED_LOOP';
+command_line = [' --property ' property ' --realization ' realization ' --x-size ' num2str(xsize) ' --bmc ' bmc ' --solver ' solver ' --connection-mode ' c_mode];
+dsv_verification(command_line);
+
+%report the verification
+output = dsv_report('output.out');
+disp(output);
 
 end
 
