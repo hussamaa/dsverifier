d19cf2719aeb76425cf59b81626f5a24fddc93fe
lennonchaves <lennon.correach@gmail.com>
2016-10-26 02:35:31 -0400

    Support for state space systems

--- matlab-toolbox/verify_ss_controllability.m
+++ matlab-toolbox/verify_ss_controllability.m
@@ -1,4 +1,17 @@
-function output = verify_ss_controllability(system, bmc, realization, solver, xsize)
+function verify_ss_controllability(system, bmc, realization, solver, xsize)
+
+global property;
+%setting the DSVERIFIER_HOME
+dsv_setup();
+%translate to ANSI-C file
+dsv_parser(system,'ss',0);
+%verifying using DSVerifier command-line
+property = 'CONTROLLABILITY';
+command_line = [' --property ' property ' --realization ' realization ' --x-size ' num2str(xsize) ' --bmc ' bmc ' --solver ' solver];
+dsv_verification(command_line,'ss');
+%report the verification
+output = dsv_report('output.out','ss');
+disp(output);
 
 end
 
--- matlab-toolbox/verify_ss_quantization_error.m
+++ matlab-toolbox/verify_ss_quantization_error.m
@@ -1,4 +1,16 @@
-function output = verify_ss_quantization_error(system, bmc, realization, solver, xsize, error)
+function verify_ss_quantization_error(system, bmc, realization, solver, xsize, error)
 
+global property;
+%setting the DSVERIFIER_HOME
+dsv_setup();
+%translate to ANSI-C file
+dsv_parser(system,'ss',error);
+%verifying using DSVerifier command-line
+property = 'STATE_SPACE_QUANTIZATION_ERROR';
+command_line = [' --property ' property ' --realization ' realization ' --x-size ' num2str(xsize) ' --bmc ' bmc ' --solver ' solver];
+dsv_verification(command_line,'ss');
+%report the verification
+output = dsv_report('output.out','ss');
+disp(output);
 end
 
