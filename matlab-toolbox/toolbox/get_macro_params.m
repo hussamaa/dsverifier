def541fb5a0bdcf3a1f51623525d01579960e5f9
lennonchaves <lennon.correach@gmail.com>
2016-10-30 12:37:08 -0400

    bmc as optional parameter

--- matlab-toolbox/toolbox/get_macro_params.m
+++ matlab-toolbox/toolbox/get_macro_params.m
@@ -19,40 +19,46 @@ if(strcmp(type,'tf') || strcmp(type,'ss'))
 elseif strcmp(type,'cl')
   nvar= n -1;
 end
-
+bmc = ' --bmc ';
 solver = ' --solver ';
 omode = ' --overflow-mode ';
 rmode = ' --rounding-mode ';
 emode = ' --error-mode ';
 timeout = ' --timeout ';
 
-if nvar >= 5
+if nvar >= 4
 if length(var{1}) > 0
-extra_param = [extra_param solver var{1}];
+extra_param = [extra_param bmc var{1}];
+end
+end
+
+if nvar >= 5
+if length(var{2}) > 0
+extra_param = [extra_param solver var{2}];
 end
 end
 
 if nvar >= 6
-if length(var{2}) > 0
-extra_param = [extra_param omode var{2}];
+if length(var{3}) > 0
+extra_param = [extra_param omode var{3}];
 end
 end
 
 if nvar >= 7
-if length(var{3}) > 0
-extra_param = [extra_param rmode var{3}];
+if length(var{4}) > 0
+extra_param = [extra_param rmode var{4}];
 end
 end
 
 if nvar >= 8
-if length(var{4}) > 0
-extra_param = [extra_param emode var{4}];
+if length(var{5}) > 0
+extra_param = [extra_param emode var{5}];
 end
 end
 
 if nvar >= 9
-if length(var{5}) > 0
-extra_param = [extra_param timeout var{5}];
+if length(var{6}) > 0
+extra_param = [extra_param timeout var{6}];
 end
 end
 
