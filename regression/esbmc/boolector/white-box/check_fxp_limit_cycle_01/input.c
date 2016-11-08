7ee900ee47c478c7f83d2fdc804df8e35ad0fd60
lennonchaves <lennon.correach@gmail.com>
2016-11-08 01:21:50 -0400

    update regression test cases

--- regression/esbmc/boolector/white-box/check_fxp_limit_cycle_01/input.c
+++ regression/esbmc/boolector/white-box/check_fxp_limit_cycle_01/input.c
@@ -3,5 +3,5 @@
 int unit_test(){
 	fxp_t y[17] = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 };
 	int y_size = 17;
-	fxp_check_persistent_limit_cycle(y, y_size);
+	fxp_check_exhaustively_limit_cycle(y, y_size);
 }
