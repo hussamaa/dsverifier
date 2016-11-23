04dcc225cbb9f592f78b49527cef19eb0a8e801f
dmdmello <dani-dmello@hotmail.com>
2016-11-23 13:01:50 -0400

    Added phase response generator

--- bmc/engine/verify_magnitude_and_phase.h
+++ bmc/engine/verify_magnitude_and_phase.h
@@ -12,30 +12,27 @@
  * ------------------------------------------------------
  */
 
-
-
-/*
- *  Generates mag response from transfer function
- */
-
 #define M_PI     3.14159265358979323846
 #include "filter_functions.h"
 
+/*
+ *  Generates magnitude response from transfer function
+ */
 void resp_mag(double* num, int lnum, double* den, int lden, double* res, int N) {
 	
 	double w;
 	int m, i;
 	double out_numRe[N + 1], 
 	double out_numIm[N + 1];
-	double out_denRee[N + 1];
-	double out_denImm[N + 1];
-	double old_out_Ree;
+	double out_denRe[N + 1];
+	double out_denIm[N + 1];
+	double old_out_Re;
 
 	for (w = 0, i = 0; w <= M_PI; w += M_PI / N, ++i) {
 		out_numRe[i] = num[0];
 		out_numIm[i] = 0;
 		for (m = 1; m < lnum; ++m) {
-			old_out_Re = out_r[i];
+			old_out_Re = out_numRe[i];
 			out_numRe[i] = cos(w) * out_numRe[i] - sin(w) * out_numIm[i] + num[m];
 			out_numIm[i] = sin(w) * old_out_Re + cos(w) * out_numIm[i];
 		}
@@ -48,8 +45,40 @@ void resp_mag(double* num, int lnum, double* den, int lden, double* res, int N) 
 			out_denIm[i] = sin(w) * old_out_Re + cos(w) * out_denIm[i];
 		}
 
-		res[i] = sqrt(out_r[i] * out_r[i] + out_i[i] * out_i[i]); 
+		res[i] = sqrt(out_numRe[i] * out_numRe[i] + out_numIm[i] * out_numIm[i]); 
 		res[i] = res[i] / sqrt(out_denRe[i] * out_denRe[i] + out_denIm[i] * out_denIm[i]); 
 
+	}
+}
+
+/*
+ *  Generates phase response from transfer function
+ */
+void resp_phase(double* num, int lnum, double* den, int lden, double* res, int N) {
+	
+	double w;
+	int m, i;
+	double out_numRe[N + 1], old_out_r;
+	double out_numIm[N + 1];
+	double out_denRe[N + 1], out_denIm[N + 1];
+	for (w = 0, i = 0; w <= M_PI; w += M_PI / N, ++i) {
+		out_numRe[i] = num[0];
+		out_numIm[i] = 0;
+		for (m = 1; m < lnum; ++m) {
+			old_out_r = out_numRe[i];
+			out_numRe[i] = cos(w) * out_numRe[i] - sin(w) * out_numIm[i] + num[m];
+			out_numIm[i] = sin(w) * old_out_r + cos(w) * out_numIm[i];
+		}
+
+		out_denRe[i] = den[0];
+		out_denIm[i] = 0;
+		for (m = 1; m < lden; ++m) { 
+			old_out_r = out_denRe[i];
+			out_denRe[i] = cos(w) * out_denRe[i] - sin(w) * out_denIm[i] + den[m];
+			out_denIm[i] = sin(w) * old_out_r + cos(w) * out_denIm[i];
+		}
+
+		res[i] = atan2(out_numIm[i], out_numRe[i]); //numerator abs
+		res[i] = res[i] - atan2(out_denIm[i], out_denRe[i]); //den abs
 	}
 }
