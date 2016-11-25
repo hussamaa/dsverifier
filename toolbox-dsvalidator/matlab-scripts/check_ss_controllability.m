8580ed6597fb60bbcb5e2b0ca26a4c221c513d16
Lennon <lennon.correach@gmail.com>
2016-11-25 18:01:56 -0400

    updating validation for state-space systems

--- toolbox-dsvalidator/matlab-scripts/check_ss_controllability.m
+++ toolbox-dsvalidator/matlab-scripts/check_ss_controllability.m
@@ -1,22 +1,24 @@
-function decision = check_ss_controllability (A,B,C,D,ts)
+function decision = check_ss_controllability (sys)
 % 
-% CHECK_SS_CONTROLLABILITY(A,B,C,D,ts)
+% CHECK_SS_CONTROLLABILITY(sys)
 % 
-% For a LTI system in state-space format, CHECK_SS_CONTROLLABILITY(A,B,C,D,ts)
+% For a LTI system in state-space format, CHECK_SS_CONTROLLABILITY(sys)
 % decides about the controlability. 
 % It returns decision = 1 if the system is controllable, and 
 % returns decision = 0 in other case.
 % 
 % Lennon Chaves
-% October 28, 2016
+% November 25, 2016
 % Manaus
 
-sys = ss(A,B,C,D);
-sys = c2d(sys,ts,'zoh');
+A = sys.A;
+B = sys.B;
+C = sys.C;
+D = sys.D;
 
-[r,c] = size(sys.A);
+[r,c] = size(A);
 
-ctrb_matrix = ctrb(sys.A,sys.B);
+ctrb_matrix = ctrb(A,B);
 n = rank(ctrb_matrix);
 
 if n == c
