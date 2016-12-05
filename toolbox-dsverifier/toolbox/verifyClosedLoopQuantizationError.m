9aee532659ad219379c9840cf08f43eeceb4b956
Lennon <lennon.correach@gmail.com>
2016-12-05 13:24:23 -0400

    refactoring closed-loop systems

--- toolbox-dsverifier/toolbox/verifyClosedLoopQuantizationError.m
+++ toolbox-dsverifier/toolbox/verifyClosedLoopQuantizationError.m
@@ -1,30 +1,40 @@
-function verify_cl_quantization_error(system, realization, xsize, c_mode, error, varargin)
+function verifyClosedLoopQuantizationError(controller, plant, intBits, fracBits, rangeMax, rangeMin, realization, kbound, error, connectionMode, varargin)
 %
 % Checks quantization error property violation for closed-loop digital systems using a bounded model checking tool.
-% Function: VERIFY_CL_QUANTIZATION_ERROR(system, realization, xsize, c_mode, error)
+% Function: verifyClosedLoopQuantizationError(controller, plant, intBits, fracBits, rangeMax, rangeMin, realization, kbound, error, connectionMode)
 %
 % Where
-%   system: represents a struct with digital system represented in transfer-function  for closed-loop systems;
+%   controller: represents a controller represented in transfer-function;
+%   plant: represents a plant represented in transfer-function;
+%   intBits: represents the integer bits part;
+%   fracBits: represents the fractionary bits part;
+%   rangeMax: represents the maximum dynamical range;
+%   rangeMin: represents the minimum dynamical range;
 %   realization: set the realization for the Digital-System (DFI, DFII, TDFII, DDFI, DDFII, and TDDFII);
-%   xsize:  set the bound of verification;
-%   c_mode: set the connection mode for the closed-loop system (SERIES or FEEDBACK);
-%   error: set the error limit.
+%   kbound: sets the k bound of verification;
+%   connectionMode: set the connection mode for the closed-loop system (SERIES or FEEDBACK);
+%   error: represents the maximum error allowed.
 %
-%  The 'system' must be structed as follow:
-%  system.system.controller = TF(den,num,ts): controller for closed-loop digital system (or use C2D for digital systems that must be discretized) ;
-%  system.system.plant = TF(den,num,ts): plant for closed-loop digital system (or use C2D for digital systems that must be discretized);
+%  The 'controller' and 'plant' must be structed as follow:
+%  system = tf(den,num,ts): transfer function representation (den - denominator, num - numerator, ts - sample time);
+%  or system = c2d(sys,ts): if the digital system should be discretized
 %  
 %  See also TF and C2D.
 %
-%  system.impl.int_bits: integer bits implementation;
-%  system.impl.frac_bits: fractional bits representation;
-%  system.range.max = max dynamical range;
-%  system.range.min = min dynamical range;
-%  system.delta = delta operator (it must be '0' if it is not a delta realization).
+% For Delta Verification, the delta operator must be informed as:
+%
+% verifyClosedLoopQuantizationError(controller, plant, intBits, fracBits, rangeMax, rangeMin, realization, kbound, error, connectionMode, delta)
+%
+% Where
+%   delta: the delta operator for a delta realization (DDFI, DDFII or TDDFII)
 %
 % Another usage form is adding other parameters (optional parameters) as follow:
 %
-% VERIFY_CL_QUANTIZATION_ERROR(system, realization, xsize, error, bmc, solver, ovmode, rmode, emode, timeout)
+% verifyClosedLoopQuantizationError(controller, plant, intBits, fracBits, rangeMax, rangeMin, realization, kbound, error, connectionMode, bmc, solver, ovmode, rmode, emode, timeout)
+%
+% For delta realization:
+%
+% verifyClosedLoopQuantizationError(controller, plant, intBits, fracBits, rangeMax, rangeMin, realization, kbound, error, connectionMode, delta, bmc, solver, ovmode, rmode, emode, timeout)
 %
 % Where
 %  bmc: set the BMC back-end for DSVerifier (ESBMC or CBMC);
@@ -37,41 +47,42 @@ function verify_cl_quantization_error(system, realization, xsize, c_mode, error,
 % Example of usage:
 %  numplant = [...];
 %  denplant = [...];
-%  ds.system.plant = tf(denplant,numplant,ts);
+%  plant = tf(denplant,numplant,ts);
 %  numcontrol = [...];
 %  dencontrol = [...];
-%  ds.system.controller = tf(dencontrol,numcontrol,ts);
-%  ds.impl.int_bits = ...;
-%  ds.impl.frac_bits = ...;
-%  ds.range.max = ...;
-%  ds.range.min = -...;
-%  ds.delta = ...;
+%  controller = tf(dencontrol,numcontrol,ts);
 %
-%  VERIFY_CL_QUANTIZATION_ERROR(ds,'DFI',10, 0.18,'CBMC');
-%  VERIFICATION FAILED!
+%  verifyClosedLoopQuantizationError(controller, plant, 2, 10, 1, -1, 'DFI', 0.18, 'SERIES');
+%  VERIFICATION SUCCESSFUL!
 %
 % Author: Lennon Chaves
 % Federal University of Amazonas
-% October, 2016
+% December 2016
 %
-global property;
 
+global property;
 %setting the DSVERIFIER_HOME
-dsv_setup();
+verificationSetup();
+
+%generating struct with sytem and its implementations.
+digitalSystem.controller = controller;
+digitalSystem.plant = plant;
+digitalSystem.impl.frac_bits = fracBits;
+digitalSystem.impl.int_bits = intBits;
+digitalSystem.range.max = rangeMax;
+digitalSystem.range.min = rangeMin;
 
 %translate to ANSI-C file
-dsv_parser(system,'cl', error,realization);
+verificationParser(digitalSystem,'cl',error,realization);
 
 %verifying using DSVerifier command-line
 property = 'QUANTIZATION_ERROR_CLOSED_LOOP';
-
-extra_param = get_macro_params(nargin-1,varargin,'cl');
-
-command_line = [' --property ' property ' --realization ' realization ' --x-size ' num2str(xsize) ' --connection-mode ' c_mode extra_param];
-dsv_verification(command_line,'cl');
+extra_param = getExtraParams(nargin,varargin,'cl',property);
+command_line = [' --property ' property ' --realization ' realization ' --x-size ' num2str(kbound) ' --connection-mode ' connectionMode extra_param];
+verificationExecution(command_line,'cl');
 
 %report the verification
-output = dsv_report('output.out','cl');
+output = verificationReport('output.out','cl');
 disp(output);
 
 end
--- toolbox-dsverifier/toolbox/verifyClosedLoopStability.m
+++ toolbox-dsverifier/toolbox/verifyClosedLoopStability.m
@@ -1,29 +1,38 @@
-function verify_cl_stability(system, realization, xsize, c_mode, varargin)
+function verifyClosedLoopStability(controller, plant, intBits, fracBits, rangeMax, rangeMin, realization, connectionMode, varargin)
 %
-% Checks robust stability property violation for closed-loop digital systems using a bounded model checking tool.
-% Function: VERIFY_CL_STABILITY(system, realization, xsize, c_mode)
+% Checks stability property violation for closed-loop digital systems using a bounded model checking tool.
+% Function: verifyClosedLoopStability(controller, plant, intBits, fracBits, rangeMax, rangeMin, realization, connectionMode)
 %
 % Where
-%   system: represents a struct with digital system represented in transfer-function for closed-loop systems;
+%   controller: represents a controller represented in transfer-function;
+%   plant: represents a plant represented in transfer-function;
+%   intBits: represents the integer bits part;
+%   fracBits: represents the fractionary bits part;
+%   rangeMax: represents the maximum dynamical range;
+%   rangeMin: represents the minimum dynamical range;
 %   realization: set the realization for the Digital-System (DFI, DFII, TDFII, DDFI, DDFII, and TDDFII);
-%   xsize:  set the bound of verification;
-%   c_mode: set the connection mode for the closed-loop system (SERIES or FEEDBACK);
+%   connectionMode: set the connection mode for the closed-loop system (SERIES or FEEDBACK);
 %
-%  The 'system' must be structed as follow:
-%  system.system.controller = TF(den,num,ts): controller for closed-loop digital system (or use C2D for digital systems that must be discretized) ;
-%  system.system.plant = TF(den,num,ts): plant for closed-loop digital system (or use C2D for digital systems that must be discretized);
+%  The 'controller' and 'plant' must be structed as follow:
+%  system = tf(den,num,ts): transfer function representation (den - denominator, num - numerator, ts - sample time);
+%  or system = c2d(sys,ts): if the digital system should be discretized
 %  
 %  See also TF and C2D.
 %
-%  system.impl.int_bits: integer bits implementation;
-%  system.impl.frac_bits: fractional bits representation;
-%  system.range.max = max dynamical range;
-%  system.range.min = min dynamical range;
-%  system.delta = delta operator (it must be '0' if it is not a delta realization).
+% For Delta Verification, the delta operator must be informed as:
+%
+% verifyClosedLoopStability(controller, plant, intBits, fracBits, rangeMax, rangeMin, realization, connectionMode, delta)
+%
+% Where
+%   delta: the delta operator for a delta realization (DDFI, DDFII or TDDFII)
 %
 % Another usage form is adding other parameters (optional parameters) as follow:
 %
-% VERIFY_CL_STABILITY(system, realization, xsize, bmc, solver, ovmode, rmode, emode, timeout)
+% verifyClosedLoopStability(controller, plant, intBits, fracBits, rangeMax, rangeMin, realization, connectionMode, bmc, solver, ovmode, rmode, emode, timeout)
+%
+% For delta realization:
+%
+% verifyClosedLoopStability(controller, plant, intBits, fracBits, rangeMax, rangeMin, realization, connectionMode, delta, bmc, solver, ovmode, rmode, emode, timeout)
 %
 % Where
 %  bmc: set the BMC back-end for DSVerifier (ESBMC or CBMC);
@@ -36,41 +45,42 @@ function verify_cl_stability(system, realization, xsize, c_mode, varargin)
 % Example of usage:
 %  numplant = [...];
 %  denplant = [...];
-%  ds.system.plant = tf(denplant,numplant,ts);
+%  plant = tf(denplant,numplant,ts);
 %  numcontrol = [...];
 %  dencontrol = [...];
-%  ds.system.controller = tf(dencontrol,numcontrol,ts);
-%  ds.impl.int_bits = ...;
-%  ds.impl.frac_bits = ...;
-%  ds.range.max = ...;
-%  ds.range.min = -...;
-%  ds.delta = ...;
+%  controller = tf(dencontrol,numcontrol,ts);
 %
-%  VERIFY_CL_STABILITY(ds,'DFI',10,'CBMC');
+%  verifyClosedLoopStability(controller, plant, 2, 10, 1, -1, 'DFI', 'SERIES');
 %  VERIFICATION FAILED!
 %
 % Author: Lennon Chaves
 % Federal University of Amazonas
-% October, 2016
+% December 2016
 %
-global property;
 
+global property;
 %setting the DSVERIFIER_HOME
-dsv_setup();
+verificationSetup();
+
+%generating struct with sytem and its implementations.
+digitalSystem.controller = controller;
+digitalSystem.plant = plant;
+digitalSystem.impl.frac_bits = fracBits;
+digitalSystem.impl.int_bits = intBits;
+digitalSystem.range.max = rangeMax;
+digitalSystem.range.min = rangeMin;
 
 %translate to ANSI-C file
-dsv_parser(system,'cl',0,realization);
+verificationParser(digitalSystem,'cl',0,realization);
 
 %verifying using DSVerifier command-line
 property = 'STABILITY_CLOSED_LOOP';
+extra_param = getExtraParams(nargin,varargin,'cl',property);
+command_line = [' --property ' property ' --realization ' realization ' --connection-mode ' connectionMode extra_param];
+verificationExecution(command_line,'cl');
 
-extra_param = get_macro_params(nargin,varargin,'cl');
-
-command_line = [' --property ' property ' --realization ' realization ' --x-size ' num2str(xsize) ' --connection-mode ' c_mode extra_param];
-
-dsv_verification(command_line,'cl');
 %report the verification
-output = dsv_report('output.out','cl');
+output = verificationReport('output.out','cl');
 disp(output);
 
 end
