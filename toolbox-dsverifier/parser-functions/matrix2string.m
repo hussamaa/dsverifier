4ec87123089751bba96b6a6b7d9de559481f9519
Lennon <lennon.correach@gmail.com>
2016-12-14 11:24:27 -0400

    parser matlab system to c/ss file

---
+++ toolbox-dsverifier/parser-functions/matrix2string.m
@@ -0,0 +1,30 @@
+function output = matrix2string( matrix )
+% Translate a matrix representation to a string representation
+%
+% Function: output = MATRIX2STRING( matrix )
+%
+%  poly: the matrix format, in a MATLAB matrix.
+%
+% Author: Lennon Chaves
+% Federal University of Amazonas
+% October 2016
+%
+
+[r,c]= size(matrix);
+linha = '[';
+for i=1:r
+    for j=1:c
+        element = matrix(i,j);
+        linha = strcat(linha,num2str(element));
+        if(j ~= c)
+         linha = strcat(linha,',');
+        end
+    end
+       if(i ~= r)
+         linha = strcat(linha,';');
+       end
+end
+linha = strcat(linha,']');
+output = linha;
+end
+
---
+++ toolbox-dsverifier/parser-functions/parserUncertainty.m
@@ -0,0 +1,53 @@
+function parserUncertainty (plant, controller, plant_uncertainty, controller_uncertainty, intBits, fracBits, minRange, maxRange, error, name)
+
+ac = cell2mat(controller.Denominator);
+nAc = length(ac);
+bc = cell2mat(controller.Numerator);
+nBc = length(bc);
+
+ap = cell2mat(plant.Denominator);
+nAp = length(ap);
+bp = cell2mat(plant.Numerator);
+nBp = length(bp);
+
+sample_time = controller.Ts;
+frac_bits = fracBits;
+int_bits = intBits;
+max_range = maxRange;
+min_range = minRange;
+
+file_name = [name '.c'];
+
+fid = fopen(file_name, 'wt' );
+fprintf(fid,'%s\n\n', '#include <dsverifier.h>');
+fprintf(fid,'%s\n\t', 'digital_system controller = { ');
+fprintf(fid,'%s %s },\n\t','.b = { ', poly2strc(bc));
+fprintf(fid,'%s %s },\n\t','.b_uncertainty = { ', poly2strc(controller_uncertainty));
+fprintf(fid,'%s %d,\n\t','.b_size = ', nBc);
+fprintf(fid,'%s %s },\n\t','.a = { ', poly2strc(ac));
+fprintf(fid,'%s %s },\n\t','.a_uncertainty = { ', poly2strc(controller_uncertainty));
+fprintf(fid,'%s %d,\n\t','.a_size = ', nAc);
+fprintf(fid,'%s %d\n','.sample_time =', sample_time);
+fprintf(fid,'%s\n\n','};');
+fprintf(fid,'%s\n\t','implementation impl = { ');
+if length(error) > 0
+    if error ~= 0
+    fprintf(fid,'%s %f,\n\t','.max_error = ', error);
+    end
+end
+fprintf(fid,'%s %d,\n\t','.int_bits = ', int_bits);
+fprintf(fid,'%s %d,\n\t','.frac_bits =  ', frac_bits);
+fprintf(fid,'%s %f,\n\t','.max = ', max_range);
+fprintf(fid,'%s %f\n\t','.min = ', min_range);
+fprintf(fid,'%s\n\n','};');
+fprintf(fid,'%s\n\t', 'digital_system plant = { ');
+fprintf(fid,'%s %s },\n\t','.b = { ', poly2strc(bp));
+fprintf(fid,'%s %s },\n\t','.b_uncertainty = { ', poly2strc(plant_uncertainty));
+fprintf(fid,'%s %d,\n\t','.b_size = ', nBp);
+fprintf(fid,'%s %s },\n\t','.a = { ', poly2strc(ap));
+fprintf(fid,'%s %d \n\t','.a_size = ', nAp);
+fprintf(fid,'%s %s },\n\t','.a_uncertainty = { ', poly2strc(plant_uncertainty));
+fprintf(fid,'%s\n\n','};');
+fclose(fid);
+
+end
