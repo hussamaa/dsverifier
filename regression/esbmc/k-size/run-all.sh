7b99c030a14624f080c856749b9c5d551aef75ef
Lennon <lennon.correach@gmail.com>
2016-12-29 10:10:50 -0400

    update esbmc test cases

---
+++ regression/esbmc/k-size/run-all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+total_time=0
+
+MODULES="digital-system overflow limit-cycle"
+
+echo ""
+echo "Script for DSVerifier started at:" $(date +"%T")
+echo ""
+
+for module in $MODULES; do
+  echo "============================== "
+  echo -n "Running" $module...
+  cd $module
+  make clean > /dev/null
+  START=$(date +"%s")
+  make 
+  END=$(date +"%s")
+  echo "Done!"
+  make clean > /dev/null 
+  cd ..
+  echo "Time elapsed for" $module ":" $(( $END - $START )) "s"
+  time=$(( $END - $START ))
+  total_time=`echo $total_time + $time | bc -l`
+  echo "Total time elapsed: " $total_time "s"
+done
+echo "============================== "
+
+echo ""
+echo "Script DSVerifier ended at:" $(date +"%T")
+echo ""
+
+
