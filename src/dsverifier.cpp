ca470d52899d68a1004c890c84d9eda60dbab729
Lucas Cordeiro <lucasccordeiro@gmail.com>
2017-01-05 16:09:02 +0000

    changed the dsverifier folder structure
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- src/dsverifier.cpp
+++ src/dsverifier.cpp
@@ -38,8 +38,6 @@
 # --------------------------------------------------
 */
 
-#define DSVERIFIER_VERSION "2.0.2"
-
 #include <iostream>
 #include <stdlib.h>
 #include <string>
@@ -56,6 +54,14 @@
 #include <streambuf>
 #include <math.h>
 
+/* eigen dependencies */
+#include <Eigen/Eigenvalues>
+#include <unsupported/Eigen/Polynomials>
+
+/* boost dependencies */
+#include <boost/algorithm/string.hpp>
+
+
 typedef bool _Bool;
 
 void __DSVERIFIER_assume(_Bool expression)
@@ -71,22 +77,18 @@ void __DSVERIFIER_assert(_Bool expression)
   }
 }
 
-#include "bmc/core/definitions.h"
-#include "bmc/core/fixed-point.h"
-#include "bmc/core/util.h"
-#include "bmc/core/delta-operator.h"
-#include "bmc/core/initialization.h"
+#include "version.h"
 
-#include "utils/print_messages.h"
+#include "../bmc/core/definitions.h"
+#include "../bmc/core/fixed-point.h"
+#include "../bmc/core/util.h"
+#include "../bmc/core/delta-operator.h"
+#include "../bmc/core/initialization.h"
 
-/* eigen dependencies */
-#include <Eigen/Eigenvalues>
-#include <unsupported/Eigen/Polynomials>
+#include "print_messages.h"
+
 typedef Eigen::PolynomialSolver<double, Eigen::Dynamic>::RootType RootType;
 typedef Eigen::PolynomialSolver<double, Eigen::Dynamic>::RootsType RootsType;
-
-/* boost dependencies */
-#include <boost/algorithm/string.hpp>
 
 const char * properties [] = { "OVERFLOW", "LIMIT_CYCLE",
 		"ZERO_INPUT_LIMIT_CYCLE", "ERROR",
@@ -125,51 +127,6 @@ bool k_induction = false;
 digital_system_state_space _controller;
 double desired_quantization_limit = 0.0;
 bool show_counterexample_data = false;
-
-/*******************************************************************\
-
-Function: help
-
-  Inputs:
-
- Outputs:
-
- Purpose:
-
-\*******************************************************************/
-
-void help ()
-{
-  std::cout << std::endl;
-  std::cout << "* * *           DSVerifier " << DSVERIFIER_VERSION << "          * * *" << std::endl;
-  std::cout << "" << std::endl;
-  std::cout << "Usage:                       Purpose:" << std::endl;
-  std::cout << "" << std::endl;
-  std::cout << "dsverifier [-h] [--help]           show help" << std::endl;
-  std::cout << "dsverifier file.c ...              source file name" << std::endl;
-  std::cout << "" << std::endl;
-  std::cout << "Options:" << std::endl;
-  std::cout << "" << std::endl;
-  std::cout << "--realization <r>            set the realization for the Digital-System" << std::endl;
-  std::cout << "                             (for Digital-Systems: DFI, DFII, TDFII, DDFI, DDFII, and TDDFII)" << std::endl;
-  std::cout << "                             (for Digital-Systems in Closed-loop: DFI, DFII, and TDFII)" << std::endl;
-  std::cout << "--property <p>               set the property to check in order to find violations" << std::endl;
-  std::cout << "                             (for Digital-Systems: OVERFLOW, LIMIT_CYCLE, ZERO_INPUT_LIMIT_CYCLE, ERROR, TIMING, STABILITY, and MINIMUM_PHASE)" << std::endl;
-  std::cout << "                             (for Digital-Systems in Closed-loop: STABILITY_CLOSED_LOOP, LIMIT_CYCLE_CLOSED_LOOP, and QUANTIZATION_ERROR_CLOSED_LOOP)" << std::endl;
-  std::cout << "--x-size <k>                 set the bound of verification" << std::endl;
-  std::cout << "--unlimited-x-size           configure the verifier to employ k-induction verification (for ESBMC only)" << std::endl;
-  std::cout << "--rounding-mode <rm>         set the rounding mode (ROUNDING, FLOOR, or CEIL)" << std::endl;
-  std::cout << "--error-mode <em>            set the error mode (ABSOLUTE or RELATIVE), default is RELATIVE" << std::endl;
-  std::cout << "--overflow-mode <om>         set the overflow mode (DETECT_OVERFLOW, SATURATE, or WRAPAROUND)" << std::endl;
-  std::cout << "--connection-mode <cm>       set the connection mode for the closed-loop system (SERIES or FEEDBACK)" << std::endl;
-  std::cout << "--bmc <b>                    set the BMC back-end for DSVerifier (ESBMC or CBMC, default is CBMC)" << std::endl;
-  std::cout << "--solver <s>                 use the specified solver in BMC back-end (e.g., boolector, z3, yices, cvc4, and minisat)" << std::endl;
-  std::cout << "--timeout <t>                configure time limit, integer followed by {s,m,h} (for ESBMC only)" << std::endl;
-  std::cout << "--tf2ss                      converts a transfer function representation of a given system to an equivalent state-space representation" << std::endl;
-  std::cout << "--show-ce-data               shows initial states, inputs, and outputs extracted from counterexample" << std::endl;
-  std::cout << "" << std::endl;
-  exit(0);
-}
 
 /*******************************************************************\
 
@@ -594,24 +551,6 @@ void validate_filename(std::string file)
 
 /*******************************************************************\
 
-Function: show_required_argument_message
-
-  Inputs:
-
- Outputs:
-
- Purpose:
-
-\*******************************************************************/
-
-void show_required_argument_message(std::string parameter)
-{
-	std::cerr << parameter << " option requires one argument." << std::endl;
-	exit(1);
-}
-
-/*******************************************************************\
-
 Function: check_required_parameters
 
   Inputs:
@@ -781,6 +720,7 @@ void bind_parameters(int argc, char* argv[])
 	  }
 	}
   }
+
   check_required_parameters();
 }
 
@@ -944,72 +884,6 @@ std::string prepare_bmc_command_line_ss()
 digital_system ds;
 implementation impl;
 
-/*******************************************************************\
-
-Function: cplus_print_fxp_array_elements
-
-  Inputs:
-
- Outputs:
-
- Purpose: print array elements
-
-\*******************************************************************/
-
-void cplus_print_fxp_array_elements(const char * name, fxp_t * v, int n)
-{
-  printf("%s = {", name);
-
-  for(int i=0; i < n; i++)
-	printf(" %jd ", v[i]);
-
-  printf("}\n");
-}
-
-/*******************************************************************\
-
-Function: cplus_print_array_elements
-
-  Inputs:
-
- Outputs:
-
- Purpose: print array elements
-
-\*******************************************************************/
-
-void cplus_print_array_elements(const char * name, double * v, int n)
-{
-  printf("%s = {", name);
-
-  for(int i=0; i < n; i++)
-  {
-	printf(" %.16f ", v[i]);
-  }
-
-  printf("}\n");
-}
-
-/*******************************************************************\
-
-Function: cplus_print_array_elements_ignoring_empty
-
-  Inputs:
-
- Outputs:
-
- Purpose: print array elements
-
-\*******************************************************************/
-
-void cplus_print_array_elements_ignoring_empty(
-  const char * name,
-  double * v,
-  int n)
-{
-  if (n > 0)
-  cplus_print_array_elements(name,v,n);
-}
 
 /*******************************************************************\
 
@@ -1438,14 +1312,11 @@ void check_stability_shift_domain_using_eigen()
   std::vector<RootType> poly_roots;
   get_roots_from_polynomial(sa_fxp, ds.a_size, poly_roots);
   bool is_stable = check_shift_stability_margin(poly_roots);
+
   if (is_stable)
-  {
 	show_verification_successful();
-  }
   else
-  {
 	show_verification_failed();
-  }
 }
 
 /*******************************************************************\
