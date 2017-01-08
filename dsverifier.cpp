6440e3dd4f5e5dc8a485ae8dbad2b973b8d09c70
dmdmello <dani-dmello@hotmail.com>
2017-01-07 21:58:34 -0400

    Added two new properties: Deterministic Filter Magnitude Verifc and Non Deterministic Filter Magnitude Verific

--- dsverifier.cpp
+++ dsverifier.cpp
@@ -89,7 +89,7 @@ typedef Eigen::PolynomialSolver<double, Eigen::Dynamic>::RootsType RootsType;
 const char * properties [] = { "OVERFLOW", "LIMIT_CYCLE", "ZERO_INPUT_LIMIT_CYCLE", "ERROR",
 		"TIMING", "TIMING_MSP430", "STABILITY", "STABILITY_CLOSED_LOOP", "LIMIT_CYCLE_CLOSED_LOOP",
 		"QUANTIZATION_ERROR_CLOSED_LOOP", "MINIMUM_PHASE", "QUANTIZATION_ERROR", "CONTROLLABILITY",
-		"OBSERVABILITY", "LIMIT_CYCLE_STATE_SPACE"};
+		"OBSERVABILITY", "LIMIT_CYCLE_STATE_SPACE", "FILTER_MAGNITUDE_NON_DET", "FILTER_MAGNITUDE_DET"};
 const char * rounding [] = { "ROUNDING", "FLOOR", "CEIL" };
 const char * overflow [] = { "DETECT_OVERFLOW", "SATURATE", "WRAPAROUND" };
 const char * realizations [] = { "DFI", "DFII", "TDFII", "DDFI", "DDFII", "TDDFII" };
@@ -149,7 +149,7 @@ void help ()
 	std::cout << "                             (for Digital-Systems: DFI, DFII, TDFII, DDFI, DDFII, and TDDFII)" << std::endl;
 	std::cout << "                             (for Digital-Systems in Closed-loop: DFI, DFII, and TDFII)" << std::endl;
 	std::cout << "--property <p>               set the property to check in order to find violations" << std::endl;
-	std::cout << "                             (for Digital-Systems: OVERFLOW, LIMIT_CYCLE, ZERO_INPUT_LIMIT_CYCLE, ERROR, TIMING, STABILITY, and MINIMUM_PHASE)" << std::endl;
+	std::cout << "                             (for Digital-Systems: OVERFLOW, LIMIT_CYCLE, ZERO_INPUT_LIMIT_CYCLE, ERROR, TIMING, STABILITY, and MINIMUM_PHASE, FILTER_MAGNITUDE_NON_DET, FILTER_MAGNITUDE_DET)" << std::endl;
 	std::cout << "                             (for Digital-Systems in Closed-loop: STABILITY_CLOSED_LOOP, LIMIT_CYCLE_CLOSED_LOOP, and QUANTIZATION_ERROR_CLOSED_LOOP)" << std::endl;
 	std::cout << "--x-size <k>                 set the bound of verification" << std::endl;
 	std::cout << "--rounding-mode <rm>         set the rounding mode (ROUNDING, FLOOR, or CEIL)" << std::endl;
@@ -305,6 +305,57 @@ void extract_data_from_file()
 		{
 			current_line = replace_all_string(current_line, ".delta=", "");
 			impl.delta = std::atof(current_line.c_str());
+			continue;
+		}
+		
+		//FILTER COEFFICIENTS 
+		std::string::size_type filter_Ap = current_line.find(".Ap", 0);
+		if (filter_Ap != std::string::npos)
+		{
+			current_line = replace_all_string(current_line, ".Ap=", "");
+			filter.Ap = std::atof(current_line.c_str());
+			continue;
+		}
+		std::string::size_type filter_Ac = current_line.find(".Ac", 0);
+		if (filter_Ac != std::string::npos)
+		{
+			current_line = replace_all_string(current_line, ".Ac=", "");
+			filter.Ac = std::atof(current_line.c_str());
+			continue;
+		}
+		std::string::size_type filter_Ar = current_line.find(".Ar", 0);
+		if (filter_Ar != std::string::npos)
+		{
+			current_line = replace_all_string(current_line, ".Ar=", "");
+			filter.Ar = std::atof(current_line.c_str());
+			continue;
+		}
+		std::string::size_type filter_wp = current_line.find(".wp", 0);
+		if (filter_wp != std::string::npos)
+		{
+			current_line = replace_all_string(current_line, ".wp=", "");
+			filter.wp = std::atof(current_line.c_str());
+			continue;
+		}
+		std::string::size_type filter_wc = current_line.find(".wc", 0);
+		if (filter_wc != std::string::npos)
+		{
+			current_line = replace_all_string(current_line, ".wc=", "");
+			filter.wc = std::atof(current_line.c_str());
+			continue;
+		}
+		std::string::size_type filter_wr = current_line.find(".wr", 0);
+		if (filter_wr != std::string::npos)
+		{
+			current_line = replace_all_string(current_line, ".wr=", "");
+			filter.wr = std::atof(current_line.c_str());
+			continue;
+		}
+		std::string::size_type filter_type = current_line.find(".type", 0);
+		if (filter_type != std::string::npos)
+		{
+			current_line = replace_all_string(current_line, ".type=", "");
+			filter.type = std::atof(current_line.c_str());
 			continue;
 		}
 	}
@@ -978,6 +1029,7 @@ std::string prepare_bmc_command_line_ss()
 
 digital_system ds;
 implementation impl;
+filter_parameters filter; 
 
 /*******************************************************************\
 
@@ -1770,6 +1822,157 @@ void check_state_space_stability()
 
 /*******************************************************************\
 
+Function: generates_mag_response
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+#define M_PI 3.14159265358979323846
+#define LOWPASS 1
+#define HIGHPASS 2
+
+void generates_mag_response(double* num, int lnum, double* den, int lden, double* res, int N) {
+
+	double w;
+	int m, i;
+	double out_numRe[N + 1];
+	double out_numIm[N + 1];
+	double out_denRe[N + 1];
+	double out_denIm[N + 1];
+	double old_out_Re;
+	double zero_test;
+	for (w = 0, i = 0; w <= M_PI; w += M_PI / N, ++i) {
+		out_numRe[i] = num[0];
+		out_numIm[i] = 0;
+		for (m = 1; m < lnum; ++m) {
+			old_out_Re = out_numRe[i];
+			out_numRe[i] = cos(w) * out_numRe[i] - sin(w) * out_numIm[i] + num[m];
+			out_numIm[i] = sin(w) * old_out_Re + cos(w) * out_numIm[i];
+		}
+		out_denRe[i] = den[0];
+		out_denIm[i] = 0;
+
+		for (m = 1; m < lden; ++m) {
+			old_out_Re = out_denRe[i];
+			out_denRe[i] = cos(w) * out_denRe[i] - sin(w) * out_denIm[i] + den[m];
+			out_denIm[i] = sin(w) * old_out_Re + cos(w) * out_denIm[i];
+		}
+
+		res[i] = sqrt(out_numRe[i] * out_numRe[i] + out_numIm[i] * out_numIm[i]); 
+	    zero_test = sqrt(out_denRe[i] * out_denRe[i] + out_denIm[i] * out_denIm[i]);
+	 	//if (zero_test != 0)
+		res[i] = res[i] / zero_test;
+	}
+}
+
+/*******************************************************************\
+
+Function: check_filter_magnitude_det
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+
+void check_filter_magnitude_det()
+{
+    int freq_response_samples = 100;
+	double w;
+	double w_incr = 1.0 / freq_response_samples;
+  	double res[freq_response_samples+1];
+  	int i,j;
+  	bool response_is_valid = true;
+
+ 	/* generates magnitude response of the quantized TF, placing the result in the "res" array*/	
+ 	generates_mag_response(ds.b, ds.b_size, ds.a, ds.a_size, res, freq_response_samples);
+
+
+  	/* quantize "a" array using fxp */
+	fxp_t a_fxp[ds.a_size];
+	fxp_double_to_fxp_array(ds.a, a_fxp, ds.a_size);
+	double _a[ds.a_size];
+	fxp_to_double_array(_a, a_fxp, ds.a_size);
+
+
+  	/* quantize "b" array using fxp */
+	fxp_t b_fxp[ds.b_size];
+	fxp_double_to_fxp_array(ds.b, b_fxp, ds.b_size);
+	double _b[ds.b_size];
+	fxp_to_double_array(_b, b_fxp, ds.b_size);
+
+
+    /* generates magnitude response of the quantized TF, placing the result in the "res" array*/
+    generates_mag_response(ds.b, ds.b_size, ds.a, ds.a_size, res, freq_response_samples);	
+
+	if (filter.type == LOWPASS) { //lowpass
+		for (i = 0, w = 0; (w <= 1.0); ++i, w += w_incr) {
+			if (w <= filter.wp) {
+				if(!(res[i] >= filter.Ap)) 
+				{
+					printf("|----------------Passband Failure-------------|");
+					response_is_valid = false;
+					break;
+				} 
+			} else if (w == filter.wc) {
+				if(!(res[i] <= filter.Ac)) 
+				{
+					printf("|-------------Cutoff Frequency Failure--------|");
+					response_is_valid = false;
+					break; 
+				}
+			} else if ((w >= filter.wr) && (w <= 1)) {
+				if(!(res[i] <= filter.Ar))
+				{
+					printf("|----------------Stopband Failure-------------|");
+					response_is_valid = false;
+					break; 
+				} 
+			}
+		}
+
+	} else if (filter.type == HIGHPASS) { //highpass
+		for (i = 0, w = 0; (w <= 1.0); ++i, w += w_incr) {
+			if (w <= filter.wr) {
+				if(!(res[i] <= filter.Ar)) 
+				{
+					printf("|----------------Stopband Failure-------------|");
+					response_is_valid = false;
+					break; 
+				}
+			} else if (w == filter.wc) {
+				if(!(res[i] <= filter.Ac)) 
+				{
+					printf("|-------------Cutoff Frequency Failure--------|");
+					response_is_valid = false;
+					break; 
+				}
+			} else if ((w > filter.wp) && (w <= 1)) {
+				if(!(res[i] >= filter.Ap))
+				{
+					printf("|----------------Passband Failure-------------|");
+					response_is_valid = false;
+					break;
+				} 
+			}
+		}
+	} 
+	
+	if (response_is_valid == false)
+		show_verification_failed();
+	else 
+		show_verification_successful();
+}
+
+/*******************************************************************\
+
 Function: check_file_exists
 
   Inputs:
@@ -1789,6 +1992,7 @@ void check_file_exists()
 		exit(1);
 	}
 }
+
 
 /*******************************************************************\
 
@@ -2392,7 +2596,7 @@ int main(int argc, char* argv[])
 
 		extract_data_from_file();
 
-		if (!(is_delta_realization && is_restricted_property))
+		if (!((is_delta_realization && is_restricted_property)||(desired_property == "FILTER_MAGNITUDE_DET")))
 		{
 			std::string command_line = prepare_bmc_command_line();
 			std::cout << "Back-end Verification: " << command_line << std::endl;
@@ -2420,6 +2624,10 @@ int main(int argc, char* argv[])
 				else if (desired_property == "MINIMUM_PHASE")
 				{
 					check_minimum_phase_delta_domain();
+				}
+				else if (desired_property == "FILTER_MAGNITUDE_DET")
+				{
+					check_filter_magnitude_det();
 				}
 
                                 if(show_counterexample_data)
