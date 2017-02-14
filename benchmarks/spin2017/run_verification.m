7969c4fe83f507481887ab264908bed8f8bde988
Lennon <lennon.correach@gmail.com>
2017-02-14 08:48:05 -0400

    updating benchmarks

--- benchmarks/spin2017/run_verification.m
+++ benchmarks/spin2017/run_verification.m
@@ -4,15 +4,31 @@ clc
 
 %Running Verification for Transfer-Function:
 
+disp('Running TF Stability');
 run_verification_tf_stability
+
+disp('Running TF Minimum Phase');
 run_verification_tf_minimum_phase
+
+disp('Running TF Overflow');
 run_verification_tf_overflow
+
+disp('Running TF Limit Cycle');
 run_verification_tf_limit_cycle
+
+disp('Running TF Quantization Error');
 run_verification_tf_error
 
 %Running Verification for State-Space:
 
+disp('Running SS Stability');
 run_verification_ss_stability
+
+disp('Running SS Observability');
 run_verification_ss_observability
+
+disp('Running SS Controllability');
 run_verification_ss_controllability
+
+disp('Running SS Quantization Error');
 run_verification_ss_quantization_error
--- benchmarks/spin2017/run_verification_tf_stability.m
+++ benchmarks/spin2017/run_verification_tf_stability.m
@@ -15,82 +15,153 @@ load('benchmark_tf.mat')
 %USCG cutter Tampa Heading Angle - 8 benchmarks - uscgtampa_tf_disc
 %Magnetic Pointer - 8 benchmarks -magneticpointer_tf_disc
 
+disp('running dcmotor_tf_disc1')
 verifyStability(dcmotor_tf_disc1,12,4,1,-1,'DFI');
+disp('running dcmotor_tf_disc2')
 verifyStability(dcmotor_tf_disc2,2,14,1,-1,'DFII');
+disp('running dcmotor_tf_disc3')
 verifyStability(dcmotor_tf_disc3,10,6,1,-1,'TDFII');
+disp('running dcmotor_tf_disc4')
 verifyStability(dcmotor_tf_disc4,8,8,1,-1,'DFI');
+disp('running dcmotor_tf_disc5')
 verifyStability(dcmotor_tf_disc5,6,10,1,-1,'DFII');
+disp('running dcmotor_tf_disc6')
 verifyStability(dcmotor_tf_disc6,7,9,1,-1,'TDFII');
+disp('running dcmotor_tf_disc7')
 verifyStability(dcmotor_tf_disc7,9,7,1,-1,'DFI');
+disp('running dcmotor_tf_disc8')
 verifyStability(dcmotor_tf_disc8,11,5,1,-1,'DFII');
 
+disp('running pendulum_tf_disc1')
 verifyStability(pendulum_tf_disc1,12,4,1,-1,'DFI');
+disp('running pendulum_tf_disc2')
 verifyStability(pendulum_tf_disc2,2,14,1,-1,'DFII');
+disp('running pendulum_tf_disc3')
 verifyStability(pendulum_tf_disc3,10,6,1,-1,'TDFII');
+disp('running pendulum_tf_disc4')
 verifyStability(pendulum_tf_disc4,8,8,1,-1,'DFI');
+disp('running pendulum_tf_disc5')
 verifyStability(pendulum_tf_disc5,6,10,1,-1,'DFII');
+disp('running pendulum_tf_disc6')
 verifyStability(pendulum_tf_disc6,7,9,1,-1,'TDFII');
+disp('running pendulum_tf_disc7')
 verifyStability(pendulum_tf_disc7,9,7,1,-1,'DFI');
+disp('running pendulum_tf_disc8')
 verifyStability(pendulum_tf_disc8,11,5,1,-1,'DFII');
 
+disp('running invpendulum_cartpos_tf_disc1')
 verifyStability(invpendulum_cartpos_tf_disc1,12,4,1,-1,'DFI');
+disp('running invpendulum_cartpos_tf_disc2')
 verifyStability(invpendulum_cartpos_tf_disc2,2,14,1,-1,'DFII');
+disp('running invpendulum_cartpos_tf_disc3')
 verifyStability(invpendulum_cartpos_tf_disc3,10,6,1,-1,'TDFII');
+disp('running invpendulum_cartpos_tf_disc4')
 verifyStability(invpendulum_cartpos_tf_disc4,8,8,1,-1,'DFI');
+disp('running invpendulum_cartpos_tf_disc5')
 verifyStability(invpendulum_cartpos_tf_disc5,6,10,1,-1,'DFII');
+disp('running invpendulum_cartpos_tf_disc6')
 verifyStability(invpendulum_cartpos_tf_disc6,7,9,1,-1,'TDFII');
+disp('running invpendulum_cartpos_tf_disc7')
 verifyStability(invpendulum_cartpos_tf_disc7,9,7,1,-1,'DFI');
+disp('running invpendulum_cartpos_tf_disc8')
 verifyStability(invpendulum_cartpos_tf_disc8,11,5,1,-1,'DFII');
 
+disp('running magsuspension_tf_disc1')
 verifyStability(magsuspension_tf_disc1,12,4,1,-1,'DFI');
+disp('running magsuspension_tf_disc2')
 verifyStability(magsuspension_tf_disc2,2,14,1,-1,'DFII');
+disp('running magsuspension_tf_disc3')
 verifyStability(magsuspension_tf_disc3,10,6,1,-1,'TDFII');
+disp('running magsuspension_tf_disc4')
 verifyStability(magsuspension_tf_disc4,8,8,1,-1,'DFI');
+disp('running magsuspension_tf_disc5')
 verifyStability(magsuspension_tf_disc5,6,10,1,-1,'DFII');
+disp('running magsuspension_tf_disc6')
 verifyStability(magsuspension_tf_disc6,7,9,1,-1,'TDFII');
+disp('running magsuspension_tf_disc7')
 verifyStability(magsuspension_tf_disc7,9,7,1,-1,'DFI');
+disp('running magsuspension_tf_disc8')
 verifyStability(magsuspension_tf_disc8,11,5,1,-1,'DFII');
 
+disp('running cruise_tf_disc1')
 verifyStability(cruise_tf_disc1,12,4,1,-1,'DFI');
+disp('running cruise_tf_disc2')
 verifyStability(cruise_tf_disc2,2,14,1,-1,'DFII');
+disp('running cruise_tf_disc3')
 verifyStability(cruise_tf_disc3,10,6,1,-1,'TDFII');
+disp('running cruise_tf_disc4')
 verifyStability(cruise_tf_disc4,8,8,1,-1,'DFI');
+disp('running cruise_tf_disc5')
 verifyStability(cruise_tf_disc5,6,10,1,-1,'DFII');
+disp('running cruise_tf_disc6')
 verifyStability(cruise_tf_disc6,7,9,1,-1,'TDFII');
+disp('running cruise_tf_disc7')
 verifyStability(cruise_tf_disc7,9,7,1,-1,'DFI');
 
+disp('running tapedriver_tf_disc1')
 verifyStability(tapedriver_tf_disc1,12,4,1,-1,'DFI');
+disp('running tapedriver_tf_disc2')
 verifyStability(tapedriver_tf_disc2,2,14,1,-1,'DFII');
+disp('running tapedriver_tf_disc3')
 verifyStability(tapedriver_tf_disc3,10,6,1,-1,'TDFII');
+disp('running tapedriver_tf_disc4')
 verifyStability(tapedriver_tf_disc4,8,8,1,-1,'DFI');
+disp('running tapedriver_tf_disc5')
 verifyStability(tapedriver_tf_disc5,6,10,1,-1,'DFII');
+disp('running tapedriver_tf_disc6')
 verifyStability(tapedriver_tf_disc6,7,9,1,-1,'TDFII');
+disp('running tapedriver_tf_disc7')
 verifyStability(tapedriver_tf_disc7,9,7,1,-1,'DFI');
+disp('running tapedriver_tf_disc8')
 verifyStability(tapedriver_tf_disc8,11,5,1,-1,'DFII');
 
+disp('running helicopter_tf_disc1')
 verifyStability(helicopter_tf_disc1,12,4,1,-1,'DFI');
+disp('running helicopter_tf_disc2')
 verifyStability(helicopter_tf_disc2,2,14,1,-1,'DFII');
+disp('running helicopter_tf_disc3')
 verifyStability(helicopter_tf_disc3,10,6,1,-1,'TDFII');
+disp('running helicopter_tf_disc4')
 verifyStability(helicopter_tf_disc4,8,8,1,-1,'DFI');
+disp('running helicopter_tf_disc5')
 verifyStability(helicopter_tf_disc5,6,10,1,-1,'DFII');
+disp('running helicopter_tf_disc6')
 verifyStability(helicopter_tf_disc6,7,9,1,-1,'TDFII');
+disp('running helicopter_tf_disc7')
 verifyStability(helicopter_tf_disc7,9,7,1,-1,'DFI');
+disp('running helicopter_tf_disc8')
 verifyStability(helicopter_tf_disc8,11,5,1,-1,'DFII');
 
+disp('running uscgtampa_tf_disc1')
 verifyStability(uscgtampa_tf_disc1,12,4,1,-1,'DFI');
+disp('running uscgtampa_tf_disc2')
 verifyStability(uscgtampa_tf_disc2,2,14,1,-1,'DFII');
+disp('running uscgtampa_tf_disc3')
 verifyStability(uscgtampa_tf_disc3,10,6,1,-1,'TDFII');
+disp('running uscgtampa_tf_disc4')
 verifyStability(uscgtampa_tf_disc4,8,8,1,-1,'DFI');
+disp('running uscgtampa_tf_disc5')
 verifyStability(uscgtampa_tf_disc5,6,10,1,-1,'DFII');
+disp('running uscgtampa_tf_disc6')
 verifyStability(uscgtampa_tf_disc6,7,9,1,-1,'TDFII');
+disp('running uscgtampa_tf_disc7')
 verifyStability(uscgtampa_tf_disc7,9,7,1,-1,'DFI');
+disp('running uscgtampa_tf_disc8')
 verifyStability(uscgtampa_tf_disc8,11,5,1,-1,'DFII');
 
+disp('running magneticpointer_tf_disc1')
 verifyStability(magneticpointer_tf_disc1,12,4,1,-1,'DFI');
+disp('running magneticpointer_tf_disc2')
 verifyStability(magneticpointer_tf_disc2,2,14,1,-1,'DFII');
+disp('running magneticpointer_tf_disc3')
 verifyStability(magneticpointer_tf_disc3,10,6,1,-1,'TDFII');
+disp('running magneticpointer_tf_disc4')
 verifyStability(magneticpointer_tf_disc4,8,8,1,-1,'DFI');
+disp('running magneticpointer_tf_disc5')
 verifyStability(magneticpointer_tf_disc5,6,10,1,-1,'DFII');
+disp('running magneticpointer_tf_disc6')
 verifyStability(magneticpointer_tf_disc6,7,9,1,-1,'TDFII');
+disp('running magneticpointer_tf_disc7')
 verifyStability(magneticpointer_tf_disc7,9,7,1,-1,'DFI');
+disp('running magneticpointer_tf_disc8')
 verifyStability(magneticpointer_tf_disc8,11,5,1,-1,'DFII');
