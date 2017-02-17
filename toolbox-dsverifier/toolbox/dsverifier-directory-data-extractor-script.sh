91593c28026c5f6e3c6461d81594ee5222dc94a5
Lennon <lennon.correach@gmail.com>
2017-02-17 09:25:47 -0400

    minor changes in bencharks

--- toolbox-dsverifier/toolbox/dsverifier-directory-data-extractor-script.sh
+++ toolbox-dsverifier/toolbox/dsverifier-directory-data-extractor-script.sh
@@ -48,13 +48,8 @@ PROPERTY=$(cat $OUTPUT_FILE | grep " Property \=" | cut -d "=" -f2 );
 REALIZATION=$(cat $OUTPUT_FILE | grep " Realization \=" | cut -d "=" -f2 );
 IMPLEMENTATION=$(cat $OUTPUT_FILE | grep " Implementation \=" | cut -d "=" -f2 | sed 's/<//' | sed 's/>//' | tr ',' ' ');
 
-if [ "$PROPERTY" = " OVERFLOW" ]; then
-NUMERATOR=$(cat $OUTPUT_FILE | grep " Numerator (fixed-point) \=" | cut -d "=" -f2 | sed 's/}//' | sed 's/{//');
-DENOMINATOR=$(cat $OUTPUT_FILE | grep " Denominator (fixed-point) \=" | cut -d "=" -f2 | sed 's/}//' | sed 's/{//');
-else
 NUMERATOR=$(cat $OUTPUT_FILE | grep " Numerator  \=" | cut -d "=" -f2 | sed 's/}//' | sed 's/{//');
 DENOMINATOR=$(cat $OUTPUT_FILE | grep " Denominator  \=" | cut -d "=" -f2 | sed 's/}//' | sed 's/{//');
-fi
 
 INPUTS=$(cat $OUTPUT_FILE | grep " Inputs =" | cut -d "=" -f2 | sed 's/}//' | sed 's/{//');
 INITIAL_STATES=$(cat $OUTPUT_FILE | grep " Initial States =" | cut -d "=" -f2 | sed 's/}//' | sed 's/{//');
