76bf797a005aa4672fe307796639abdf64da1739
Lennon <lennon.correach@gmail.com>
2017-02-21 06:28:30 -0400

    updating benchmarks

--- benchmarks/spin2017/run_cl_verification.m
+++ benchmarks/spin2017/run_cl_verification.m
@@ -3,7 +3,7 @@ disp('Verifying Closed-Loop Stability')
 for i=1:length(digital_system)
     message = ['Verifying Digital System ' num2str(i)];
     disp(message)
-    verifyClosedLoopStability(digital_system(i).controller, digital_system(i).plant, digital_system(i).int_bits, digital_system(i).frac_bits, digital_system(i).rangeMax, digital_system(i).rangeMin, digital_system(i).realization, 'SERIES');
+    verifyClosedLoopStability(digital_system(i).controller, digital_system(i).plant, digital_system(i).int_bits, digital_system(i).frac_bits, digital_system(i).rangeMax, digital_system(i).rangeMin, digital_system(i).realization, 'SERIES', 'ESBMC','','','','',3600);
 end
 
 disp('Verifying Closed-Loop Limit-Cycle')
@@ -11,7 +11,7 @@ disp('Verifying Closed-Loop Limit-Cycle')
 for i=1:length(digital_system)
     message = ['Verifying Digital System ' num2str(i)];
     disp(message)
-    verifyClosedLoopLimitCycle(digital_system(i).controller, digital_system(i).plant, digital_system(i).int_bits, digital_system(i).frac_bits, digital_system(i).rangeMax, digital_system(i).rangeMin, digital_system(i).realization, 10, 'SERIES');
+    verifyClosedLoopLimitCycle(digital_system(i).controller, digital_system(i).plant, digital_system(i).int_bits, digital_system(i).frac_bits, digital_system(i).rangeMax, digital_system(i).rangeMin, digital_system(i).realization, 10, 'SERIES', 'ESBMC','','','','',3600);
 end
 
 disp('Verifying Closed-Loop Quantization Error')
@@ -19,7 +19,7 @@ disp('Verifying Closed-Loop Quantization Error')
 for i=1:length(digital_system)
     message = ['Verifying Digital System ' num2str(i)];
     disp(message)
-    verifyClosedLoopQuantizationError(digital_system(i).controller, digital_system(i).plant, digital_system(i).int_bits, digital_system(i).frac_bits, digital_system(i).rangeMax, digital_system(i).rangeMin, digital_system(i).realization, 10, 0.18, 'SERIES');
+    verifyClosedLoopQuantizationError(digital_system(i).controller, digital_system(i).plant, digital_system(i).int_bits, digital_system(i).frac_bits, digital_system(i).rangeMax, digital_system(i).rangeMin, digital_system(i).realization, 10, 0.18, 'SERIES','','','','',3600);
 end
 
 disp('End of Verification of Closed-Loop')
