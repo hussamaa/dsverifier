ca621e500db0ee7b188bceb3a9815ac0ec8d2360
Lennon <lennon.correach@gmail.com>
2017-03-01 07:11:02 -0400

    updating test cases

--- regression/closed-loop/helicopter/input.c
+++ regression/closed-loop/helicopter/input.c
@@ -1,29 +1,24 @@
 #include <dsverifier.h>
 
-digital_system controller = {
-        .b = { 0.-0.0009765625, 0, 0 },
-        .b_uncertainty = { 0.0 , 0.0 , 0.0 },
-        .b_size = 3,
-        .a = { 0.76171875, 0, 0, 0},
-        .a_uncertainty = { 0.0, 0.0 , 0.0 , 0.0 },
-        .a_size = 4,
-        .sample_time = 2,
+digital_system controller = { 
+	.b = {  0,-0.00097656,0,0 },
+	.b_size =  4,
+	.a = {  0.76172,0,0,0 },
+	.a_size =  4,
+	.sample_time = 2
 };
 
-implementation impl = {
-        .int_bits = 3,
-        .frac_bits = 7,
-        .max = 1.0,
-        .min = -1.0,
-        .scale = 1,
-};
+implementation impl = { 
+	.int_bits =  3,
+	.frac_bits =   7,
+	.max =  1.000000,
+	.min =  -1.000000
+	};
 
-digital_system plant = {
-        .b = { 15.1315f, 17.8600f, 17.4549f },
-        .b_uncertainty = { 0.0, 0.0, 0.0 },
-        .b_size = 3,
-        .a = { 1.0f, -2.6207f, 2.3586f, -0.6570f },
-        .a_uncertainty = { 0.0, 0.0, 0.0, 0.0 },
-        .a_size = 4,
-};
+digital_system plant = { 
+	.b = {  0,15.1315,17.86,17.4549 },
+	.b_size =  4,
+	.a = {  1,-2.6207,2.3586,-0.657 },
+	.a_size =  4 
+	};
 
