8f7bf9c0f3bfb3e1a5a6fc7ea2d32670d5c1562c
Lennon <lennon.correach@gmail.com>
2017-04-19 14:16:43 -0400

    jscp benchmarks

---
+++ regression/jcsp-closed-loop/CBMC/LIMIT_CYCLE_CLOSED_LOOP/32-bits/DFII/Uncertainty-1.5/test-case-13/input.c
@@ -0,0 +1,28 @@
+#include <dsverifier.h>
+
+digital_system controller = { 
+	.b = {  379 , 39383 , 192306 , 382993 , 383284 , 192175 , 38582 },
+	.b_uncertainty = {  0.015 , 0.015 , 0.015 , 0.015 , 0.015 , 0.015 , 0.015 },
+	.b_size =  7,
+	.a = {  3 , -328 , -38048 , -179760 , -314330 , -239911 , -67626 },
+	.a_uncertainty = {  0.015 , 0.015 , 0.015 , 0.015 , 0.015 , 0.015 , 0.015 },
+	.a_size =  7,
+	.sample_time = 0
+};
+
+implementation impl = { 
+	.int_bits =  15,
+	.frac_bits =   16,
+	.max =  1.000000,
+	.min =  -1.000000
+	};
+
+digital_system plant = { 
+	.b = {  0 , 1 , -1 },
+	.b_uncertainty = {  0.015 , 0.015 , 0.015 },
+	.b_size =  3,
+	.a = {  1 , -1 , -2 },
+	.a_size =  3 
+	.a_uncertainty = {  0.015 , 0.015 , 0.015 },
+	};
+
