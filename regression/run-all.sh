81453e84f5f3532c135dac2ac2961416c6347a30
Lennon Chaves <lennon.chaves@tcl.com>
2017-08-08 07:30:42 -0400

    updated runner script

--- regression/run-all.sh
+++ regression/run-all.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 # script to run all test cases
-# TODO: include state-space test cases
+#
 # run chmod a+x run-all.sh
 # ./run-all.sh
 #
 # author: Lennon Chaves
-# November, 2016
+# August, 2017
 # Manaus, Amazonas
 #
 
 chmod a+x cbmc/run-all.sh
 chmod a+x esbmc/boolector/run-all.sh
+chmod a+x esbmc/z3/run-all.sh
+
 echo "RUNNING CBMC TESTS";
 path=$PWD
 cd $path/cbmc
 ./run-all.sh
-#echo "RUNNING ESBMC TESTS - Boolector";
-#cd $path/esbmc/boolector
-#./run-all.sh
-#echo "RUNNING ESBMC TESTS - Z3";
-#cd $path/esbmc/z3
-#make
+
+echo "RUNNING ESBMC TESTS - Boolector";
+cd $path
+cd $path/esbmc/boolector
+./run-all.sh
+
+echo "RUNNING ESBMC TESTS - Z3";
+cd $path
+cd $path/esbmc/z3
+./run-all.sh
