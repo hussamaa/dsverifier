b341826117a9dddca6181530a1db1a7287851463
Lennon Chaves <lennonc@motorola.com>
2018-05-14 15:44:53 -0400

    ALF setup

---
+++ benchmarks/tr2018/ALF/ds-04-impl3.c
@@ -0,0 +1,16 @@
+#include <dsverifier.h>
+
+digital_system ds = {
+	.b = { 135.0, -260.0, 125.0 },
+	.b_size = 3,
+	.a = { 1.0, -1.0, 0.0 },
+	.a_size = 3,
+	.sample_time = 0.02
+};
+	
+implementation impl = {
+	.int_bits = 11,
+	.frac_bits = 5,
+	.max = 1.0,
+	.min = -1.0
+};	
---
+++ benchmarks/tr2018/ALF/ds-07-impl1.c
@@ -0,0 +1,16 @@
+#include <dsverifier.h>
+
+digital_system ds = {
+	.b = { 0.1, -0.09998 },
+	.b_size = 2,
+	.a = { 1.0, -1.0 },
+	.a_size = 2,
+	.sample_time = 0.02
+};
+	
+implementation impl = {
+	.int_bits = 4,
+	.frac_bits = 12,
+	.max = 1.0,
+	.min = -1.0
+};	
